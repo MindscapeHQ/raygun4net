9fbcfc5ed1d8dbbeb3813f5404a81d8861509440
Martin Holman <martin@raygun.io>
2012-11-22 13:03:08 +1300

    basic client and config

---
+++ Mindscape.Raygun4Net/RaygunClient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+
+using Newtonsoft.Json.Linq;
+
+namespace Mindscape.Raygun4Net
+{
+  public class RaygunClient
+  {
+    public void Send(Exception exception)
+    {
+      Send(new RaygunMessage(exception));
+    }
+
+    public void Send(RaygunMessage raygunMessage)
+    {
+      using (var client = new WebClient())
+      {
+        client.Headers.Add("X-ApiKey", RaygunSettings.Settings.ApiKey);
+
+        client.UploadString(RaygunSettings.Settings.ApiEndpoint, JObject.FromObject(raygunMessage).ToString());
+      }
+    }
+  }
+}
---
+++ Mindscape.Raygun4Net/RaygunMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Mindscape.Raygun4Net
+{
+  public class RaygunMessage
+  {
+    public RaygunMessage(Exception exception)
+    {
+      OccurredOn = DateTime.UtcNow;
+      Details = new RaygunMessageDetails { Error = new RaygunErrorMessageDetails(exception) };
+    }
+
+    public DateTime OccurredOn { get; set; }
+
+    public RaygunMessageDetails Details { get; set; }
+  }
+}
