ea2c72964d99299acce6ff0a8d141f07af7769f1
Martin Holman <martin@raygun.io>
2012-12-10 15:06:54 +1300

    simple httpmodule

---
+++ Mindscape.Raygun4Net/RaygunHttpModule.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Web;
+
+namespace Mindscape.Raygun4Net
+{
+  public class RaygunHttpModule : IHttpModule
+  {
+    public void Init(HttpApplication context)
+    {
+      context.Error += SendError;
+    }
+
+    public void Dispose()
+    {
+    }
+
+    private void SendError(object sender, EventArgs e)
+    {
+      var application = (HttpApplication)sender;
+
+      var exception = application.Server.GetLastError();
+
+      var raygunClient = new RaygunClient();
+
+      raygunClient.Send(exception);
+    }
+  }
+}
