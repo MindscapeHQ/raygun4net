d1ec6b6e3fb4c7e26312685562d24443bbed8d3c
Martin Holman <martin@raygun.io>
2012-12-10 17:21:44 +1300

    split the stack trace into bits

---
+++ Mindscape.Raygun4Net.Tests/RaygunErrorMessageBuilderTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+using NUnit.Framework;
+
+namespace Mindscape.Raygun4Net.Tests
+{
+  [TestFixture]
+  public class RaygunErrorMessageBuilderTests
+  {
+    private Exception _exception;
+
+    [SetUp]
+    public void SetUp()
+    {
+      try
+      {
+        ExceptionallyCrappyMethod<string, int>("bogus");
+      }
+      catch (Exception ex)
+      {
+        _exception = ex;
+      }
+    }
+
+    private void ExceptionallyCrappyMethod<T, T2>(T bung)
+    {
+      throw new InvalidOperationException();
+    }
+
+    [Test]
+    public void ClassNameIsNotEmpty()
+    {
+      var raygunErrorMessage = RaygunErrorMessageBuilder.New.SetClassName(_exception).Build();
+
+      Assert.That(raygunErrorMessage.ClassName, Is.Not.EqualTo(string.Empty));
+    }
+
+    [Test]
+    public void StackTraceIsNotEmpty()
+    {
+      var raygunErrorMessage = RaygunErrorMessageBuilder.New.SetStackTrace(_exception).Build();
+
+      Assert.That(raygunErrorMessage.StackTrace, Is.Not.Empty);
+    }
+
+    [Test]
+    public void MessageIsNotEmpty()
+    {
+      var raygunErrorMessage = RaygunErrorMessageBuilder.New.SetMessage(_exception).Build();
+
+      Assert.That(raygunErrorMessage.Message, Is.Not.EqualTo(string.Empty));
+    }
+  }
+}
