bb979af3544772dd226c80375dcbbee8279911a7
Callum Gavin <callum@mindscape.co.nz>
2013-01-30 09:39:11 +1300

    Removed redundant properties, changed Form to NameValueCollection

--- Mindscape.Raygun4Net/Messages/RaygunRequestMessage.cs
+++ Mindscape.Raygun4Net/Messages/RaygunRequestMessage.cs
@@ -10,19 +10,35 @@ namespace Mindscape.Raygun4Net.Messages
   {
     public RaygunRequestMessage(HttpContext httpContext)
     {
-      HostName = httpContext.Request.Url.Host;
-      Url = httpContext.Request.Url.AbsolutePath;
-      HttpMethod = httpContext.Request.RequestType;
-      IPAddress = httpContext.Request.UserHostAddress;
       Data = ToDictionary(httpContext.Request.ServerVariables);
       QueryString = ToDictionary(httpContext.Request.QueryString);
       Headers = ToDictionary(httpContext.Request.Headers);
-      if (httpContext.Request.UrlReferrer != null) Referrer = httpContext.Request.UrlReferrer.ToString();
-      UserAgent = httpContext.Request.UserAgent;      
-      Form = httpContext.Request.Form.ToString().Substring(0, 256);
+      Form = new NameValueCollection();
 
-      if (httpContext.Request.Headers["Content-Type"] != "text/html" && httpContext.Request.Headers["Content-Type"]
-        != "application/x-www-form-urlencoded" && HttpMethod != "GET")
+      foreach (string s in httpContext.Request.Form)
+      {
+        string name = s;
+        string value = httpContext.Request.Form[s];
+
+        if (s.Length <= 256 && value.Length <= 256)
+        {
+          Form.Add(s, httpContext.Request.Form[s]); 
+        }
+
+        if (s.Length > 256)
+        {
+          name = s.Substring(0, 256);
+        }
+        if (value.Length > 256)
+        {
+          value = value.Substring(0, 256);
+        }
+        Form.Remove(s);
+        Form.Add(name, value);
+      }
+
+      var contentType = httpContext.Request.Headers["Content-Type"];
+      if (contentType != "text/html" && contentType != "application/x-www-form-urlencoded" && httpContext.Request.RequestType != "GET")        
       {
         int length = 4096;        
         string temp = new StreamReader(httpContext.Request.InputStream).ReadToEnd();
@@ -42,27 +58,15 @@ namespace Mindscape.Raygun4Net.Messages
       return keys.ToDictionary(s => s, s => nameValueCollection[s]);
     }    
 
-    public string HostName { get; set; }
-
-    public string Url { get; set; }
-
-    public string HttpMethod { get; set; }
-
-    public string IPAddress { get; set; }
-
-    public IDictionary QueryString { get; set; }
-
-    public IDictionary Headers { get; set; }
+    public IDictionary QueryString { get; set; }    
 
     public IDictionary Data { get; set; }
 
-    public string UserAgent { get; set; }
-
-    public string Referrer { get; set; }
-
-    public string Form { get; set; }
+    public NameValueCollection Form { get; set; }
 
     public string RawData { get; set; }
+
+    public IDictionary Headers { get; set; }
 
   }
 }
