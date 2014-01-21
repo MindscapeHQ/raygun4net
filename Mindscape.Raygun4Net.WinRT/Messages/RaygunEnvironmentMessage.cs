bb7a2d298b1f8651d5559507c91ffdaf90714778
Jason Fauchelle <quantumnightmare@hotmail.com>
2014-01-21 14:31:13 +1300

    Cleaned up the WinRT provider classes.

---
+++ Mindscape.Raygun4Net.WinRT/Messages/RaygunEnvironmentMessage.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.Graphics.Display;
+using Windows.Devices.Enumeration.Pnp;
+
+namespace Mindscape.Raygun4Net.Messages
+{
+  public class RaygunEnvironmentMessage
+  {
+    private List<double> _diskSpaceFree = new List<double>();
+
+    public RaygunEnvironmentMessage()
+    {
+      //WindowBoundsHeight = Windows.UI.Xaml.Window.Current.Bounds.Height;
+      //WindowBoundsWidth = Windows.UI.Xaml.Window.Current.Bounds.Width;
+      PackageVersion = string.Format("{0}.{1}", Package.Current.Id.Version.Major, Package.Current.Id.Version.Minor);
+      Cpu = Package.Current.Id.Architecture.ToString();
+      ResolutionScale = DisplayProperties.ResolutionScale.ToString();
+      CurrentOrientation = DisplayProperties.CurrentOrientation.ToString();
+      Location = Windows.System.UserProfile.GlobalizationPreferences.HomeGeographicRegion;
+
+      DateTime now = DateTime.Now;
+      UtcOffset = TimeZoneInfo.Local.GetUtcOffset(now).TotalHours;
+
+      SYSTEM_INFO systemInfo = new SYSTEM_INFO();
+      RaygunSystemInfoWrapper.GetNativeSystemInfo(ref systemInfo);
+      Architecture = systemInfo.wProcessorArchitecture.ToString();
+    }
+
+    private async Task<PnpObjectCollection> GetDevices()
+    {
+      string[] properties =
+        {
+          "System.ItemNameDisplay",
+          "System.Devices.ContainerId"
+        };
+
+      return await PnpObject.FindAllAsync(PnpObjectType.Device, properties);
+    }
+
+    public int ProcessorCount { get; private set; }
+
+    public string OSVersion { get; private set; }
+
+    public double WindowBoundsWidth { get; private set; }
+
+    public double WindowBoundsHeight { get; private set; }
+
+    public string ResolutionScale { get; private set; }
+
+    public string CurrentOrientation { get; private set; }
+
+    public string Cpu { get; private set; }
+
+    public string PackageVersion { get; private set; }
+
+    public string Architecture { get; private set; }
+
+    [Obsolete("Use Locale instead")]
+    public string Location { get; private set; }
+
+    public ulong TotalVirtualMemory { get; private set; }
+
+    public ulong AvailableVirtualMemory { get; private set; }
+
+    public List<double> DiskSpaceFree
+    {
+      get { return _diskSpaceFree; }
+      set { _diskSpaceFree = value; }
+    }
+
+    public ulong TotalPhysicalMemory { get; private set; }
+
+    public ulong AvailablePhysicalMemory { get; private set; }
+
+    public string DeviceName { get; private set; }
+
+    public double UtcOffset { get; private set; }
+
+    // Refactored properties
+
+    public string Locale { get; private set; }
+  }
+}
---
+++ Mindscape.Raygun4Net.WinRT/RaygunClient.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using Mindscape.Raygun4Net.Messages;
+
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using Windows.Networking.Connectivity;
+using Windows.UI.Xaml;
+
+namespace Mindscape.Raygun4Net
+{
+  public class RaygunClient
+  {
+    private readonly string _apiKey;
+    private static List<Type> _wrapperExceptions;
+    private List<string> _ignoredFormNames;
+
+    /// <summary>
+    /// Gets or sets the user identity string.
+    /// </summary>
+    public string User { get; set; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RaygunClient" /> class.
+    /// </summary>
+    /// <param name="apiKey">The API key.</param>
+    public RaygunClient(string apiKey)
+    {
+      _apiKey = apiKey;
+      _wrapperExceptions = new List<Type>();
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RaygunClient" /> class.
+    /// Uses the ApiKey specified in the config file.
+    /// </summary>
+    public RaygunClient()
+      : this(RaygunSettings.Settings.ApiKey)
+    {
+    }
+
+    private bool ValidateApiKey()
+    {
+      if (string.IsNullOrEmpty(_apiKey))
+      {
+        System.Diagnostics.Debug.WriteLine("ApiKey has not been provided, exception will not be logged");
+        return false;
+      }
+      return true;
+    }
+
+    /// <summary>
+    /// Sends the exception from an UnhandledException event to Raygun.io, optionally with a list of tags
+    /// for identification.
+    /// </summary>
+    /// <param name="unhandledExceptionEventArgs">The event args from UnhandledException, containing the thrown exception and its message.</param>
+    /// <param name="tags">An optional list of strings to identify the message to be transmitted.</param>
+    /// <param name="userCustomData">A key-value collection of custom data that is to be sent along with the message</param>
+    public void Send(UnhandledExceptionEventArgs unhandledExceptionEventArgs, [Optional] IList<string> tags, [Optional] IDictionary userCustomData)
+    {
+      if (ValidateApiKey())
+      {
+        var exception = unhandledExceptionEventArgs.Exception;
+        exception.Data.Add("Message", unhandledExceptionEventArgs.Message);
+
+        Send(CreateMessage(exception, tags, userCustomData));
+      }
+    }
+
+    /// <summary>
+    /// Sends the exception to Raygun.io, optionally with a list of tags for identification.
+    /// </summary>
+    /// <param name="exception">The exception thrown by the wrapped method</param>
+    /// <param name="tags">A list of string tags relating to the message to identify it</param>
+    /// <param name="userCustomData">A key-value collection of custom data that is to be sent along with the message</param>
+    public void Send(Exception exception, [Optional] IList<string> tags, [Optional] IDictionary userCustomData)
+    {
+      if (ValidateApiKey())
+      {
+        Send(CreateMessage(exception, tags, userCustomData));
+      }
+    }
+
+    public async void Send(RaygunMessage raygunMessage)
+    {
+      HttpClientHandler handler = new HttpClientHandler { UseDefaultCredentials = true };
+
+      var client = new HttpClient(handler);
+      {
+        client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("raygun4net-winrt", "1.0.0"));
+
+        HttpContent httpContent = new StringContent(SimpleJson.SerializeObject(raygunMessage));
+        httpContent.Headers.ContentType = new MediaTypeHeaderValue("application/x-raygun-message");
+        httpContent.Headers.Add("X-ApiKey", _apiKey);
+
+        try
+        {
+          await PostMessageAsync(client, httpContent, RaygunSettings.Settings.ApiEndpoint);
+        }
+        catch (Exception ex)
+        {
+          System.Diagnostics.Debug.WriteLine(string.Format("Error Logging Exception to Raygun.io {0}", ex.Message));
+
+          if (RaygunSettings.Settings.ThrowOnError)
+          {
+            throw;
+          }
+        }
+      }
+    }
+
+    private RaygunMessage CreateMessage(Exception exception, [Optional] IList<string> tags, [Optional] IDictionary userCustomData)
+    {
+      var message = RaygunMessageBuilder.New
+          .SetEnvironmentDetails()
+          .SetMachineName(NetworkInformation.GetHostNames()[0].DisplayName)
+          .SetExceptionDetails(exception)
+          .SetClientDetails()
+          .SetVersion()
+          .SetUser(User)
+          .Build();
+
+      if (tags != null)
+      {
+        message.Details.Tags = tags;
+      }
+      if (userCustomData != null)
+      {
+        message.Details.UserCustomData = userCustomData;
+      }
+      return message;
+    }
+
+#pragma warning disable 1998
+    private async Task PostMessageAsync(HttpClient client, HttpContent httpContent, Uri uri)
+#pragma warning restore 1998
+    {
+      HttpResponseMessage response;
+      response = client.PostAsync(uri, httpContent).Result;
+      client.Dispose();
+    }
+
+    public void Wrap(Action func, [Optional] List<string> tags)
+    {
+      try
+      {
+        func();
+      }
+      catch (Exception ex)
+      {
+        Send(ex);
+        throw;
+      }
+    }
+
+    public TResult Wrap<TResult>(Func<TResult> func, [Optional] List<string> tags)
+    {
+      try
+      {
+        return func();
+      }
+      catch (Exception ex)
+      {
+        Send(ex);
+        throw;
+      }
+    }
+  }
+}
