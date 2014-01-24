5a8f12ded3f8c7a65dfe17cd5ad121a262fb382d
Jason Fauchelle <jason@mindscape.co.nz>
2014-01-24 16:04:09 +1300

    Initial commit of Xamarin.iOS unit test suite.

---
+++ Mindscape.Raygun4Net.Xamarin.iOS.Tests/RaygunMessageBuilderTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mindscape.Raygun4Net.Messages;
+using NUnit.Framework;
+
+namespace Mindscape.Raygun4Net.Xamarin.iOS.Tests
+{
+  [TestFixture]
+  public class RaygunMessageBuilderTests
+  {
+    private RaygunMessageBuilder _builder;
+
+    [SetUp]
+    public void SetUp()
+    {
+      _builder = RaygunMessageBuilder.New;
+    }
+
+    [Test]
+    public void New()
+    {
+      Assert.IsNotNull(_builder);
+    }
+
+    [Test]
+    public void SetVersion()
+    {
+      IRaygunMessageBuilder builder = _builder.SetVersion("Custom Version");
+      Assert.AreEqual(_builder, builder);
+
+      RaygunMessage message = _builder.Build();
+      Assert.AreEqual("Custom Version", message.Details.Version);
+    }
+  }
+}
