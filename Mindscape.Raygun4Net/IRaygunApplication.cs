55be68000739a733f113715254a6079d08b19dca
Martin Holman <martin@raygun.io>
2014-05-28 12:13:28 +1200

    some comments on the use of this

--- Mindscape.Raygun4Net/IRaygunApplication.cs
+++ Mindscape.Raygun4Net/IRaygunApplication.cs
@@ -1,7 +1,14 @@
 ﻿namespace Mindscape.Raygun4Net
 {
+  /// <summary>
+  /// This is used by the HttpModule to generate RaygunClients. If you wish to
+  /// use it then, implement this interface with your HttpApplication.
+  /// </summary>
   public interface IRaygunApplication
   {
+    /// <summary>
+    /// This is called by the HttpModule to get a customised RaygunClient.
+    /// </summary>
     RaygunClient GenerateRaygunClient();
   }
 }
