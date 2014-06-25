9bd3d633cecc09f7c17b72f50884526e49a66de5
Jason Fauchelle <jason@mindscape.co.nz>
2014-06-25 17:03:00 +1200

    Added user IsAnonymous flag for Xamarin.Android.

--- Mindscape.Raygun4Net.Xamarin.Android/RaygunClient.cs
+++ Mindscape.Raygun4Net.Xamarin.Android/RaygunClient.cs
@@ -57,6 +57,11 @@ namespace Mindscape.Raygun4Net
     public string User { get; set; }
 
     /// <summary>
+    /// Gets or sets information about the user including the identity string.
+    /// </summary>
+    public RaygunIdentifierMessage UserInfo { get; set; }
+
+    /// <summary>
     /// Gets or sets a custom application version identifier for all error messages sent to the Raygun.io endpoint.
     /// </summary>
     public string ApplicationVersion { get; set; }
@@ -231,7 +236,7 @@ namespace Mindscape.Raygun4Net
         .SetVersion(ApplicationVersion)
         .SetTags(tags)
         .SetUserCustomData(userCustomData)
-        .SetUser(User)
+        .SetUser(UserInfo ?? (!String.IsNullOrEmpty(User) ? new RaygunIdentifierMessage(User) : null))
         .Build();
       return message;
     }
