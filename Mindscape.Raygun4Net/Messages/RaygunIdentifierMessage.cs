79cace5e3c4938e0e506c7ede76a933b40450abd
Jamie Penney <jamie@jamiepenney.co.nz>
2014-06-30 10:24:05 +1200

    Add doc comments to RaygunIdentifierMessage

--- Mindscape.Raygun4Net/Messages/RaygunIdentifierMessage.cs
+++ Mindscape.Raygun4Net/Messages/RaygunIdentifierMessage.cs
@@ -7,16 +7,36 @@
       Identifier = user;
     }
 
+    /// <summary>
+    /// Unique Identifier for this user. Set this to the identifier you use internally to look up users,
+    /// or a correlation id for anonymous users if you have one. It doesn't have to be unique, but we will
+    /// treat any duplicated values as the same user.
+    /// </summary>
     public string Identifier { get; set; }
 
+    /// <summary>
+    /// Flag indicating whether a user is anonymous or not.
+    /// </summary>
     public bool IsAnonymous { get; set; }
 
+    /// <summary>
+    /// User's email address
+    /// </summary>
     public string Email { get; set; }
 
+    /// <summary>
+    /// User's full name. If you are going to set any names, you should probably set this one too.
+    /// </summary>
     public string FullName { get; set; }
 
+    /// <summary>
+    /// User's first name.
+    /// </summary>
     public string FirstName { get; set; }
 
+    /// <summary>
+    /// User's last name.
+    /// </summary>
     public string LastName { get; set; }
 
   }
