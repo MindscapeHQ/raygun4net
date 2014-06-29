d00628d4d01f5faaa296d03f9851216772725821
Jamie Penney <jamie@jamiepenney.co.nz>
2014-06-30 10:11:31 +1200

    Add email, first, last, and full name to RaygunIdentifierMessage

--- Mindscape.Raygun4Net/Messages/RaygunIdentifierMessage.cs
+++ Mindscape.Raygun4Net/Messages/RaygunIdentifierMessage.cs
@@ -10,5 +10,14 @@
     public string Identifier { get; set; }
 
     public bool IsAnonymous { get; set; }
+
+    public string Email { get; set; }
+
+    public string FullName { get; set; }
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
   }
 }
