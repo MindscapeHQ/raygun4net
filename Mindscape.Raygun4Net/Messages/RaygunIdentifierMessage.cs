44970dbf630cc98a368a3194baf466bd5b3e86d2
Jamie Penney <jamie@jamiepenney.co.nz>
2014-06-30 10:42:12 +1200

    More documentation

--- Mindscape.Raygun4Net/Messages/RaygunIdentifierMessage.cs
+++ Mindscape.Raygun4Net/Messages/RaygunIdentifierMessage.cs
@@ -10,7 +10,8 @@
     /// <summary>
     /// Unique Identifier for this user. Set this to the identifier you use internally to look up users,
     /// or a correlation id for anonymous users if you have one. It doesn't have to be unique, but we will
-    /// treat any duplicated values as the same user.
+    /// treat any duplicated values as the same user. If you use the user's email address as the identifier,
+    /// enter it here as well as the Email field.
     /// </summary>
     public string Identifier { get; set; }
 
