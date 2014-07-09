b797afefe5774d0be22f8ceb737a9a17180c5273
Jason Fauchelle <jason@mindscape.co.nz>
2014-07-09 17:37:12 +1200

    Added WebApi references and classes.

---
+++ Mindscape.Raygun4Net45/WebApi/RaygunWebApiExceptionLogger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.ExceptionHandling;
+
+namespace Mindscape.Raygun4Net.WebApi
+{
+  public class RaygunWebApiExceptionLogger : ExceptionLogger
+  {
+    private readonly ICanCreateRaygunClient _clientCreator;
+
+    internal RaygunWebApiExceptionLogger(ICanCreateRaygunClient generateRaygunClient)
+    {
+      _clientCreator = generateRaygunClient;
+    }
+
+    public override void Log(ExceptionLoggerContext context)
+    {
+      _clientCreator.GetClient().CurrentHttpRequest(context.Request).Send(context.Exception, new List<string> { "Exception Logger" });
+    }
+
+    public override Task LogAsync(ExceptionLoggerContext context, CancellationToken cancellationToken)
+    {
+      return Task.Factory.StartNew(() => _clientCreator.GetClient().CurrentHttpRequest(context.Request).Send(context.Exception, new List<string> { "Exception Logger" }), cancellationToken);
+    }
+  }
+}
+
