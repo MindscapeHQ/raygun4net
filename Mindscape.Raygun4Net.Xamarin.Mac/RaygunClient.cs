6ede895ca2649faf9d33f0785797ba6dc1536db7
QuantumNightmare <quantumnightmare@hotmail.com>
2014-08-04 14:10:28 +1200

    Initial commit of Xamarin.Mac provider.

---
+++ Mindscape.Raygun4Net.Xamarin.Mac/RaygunClient.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Mindscape.Raygun4Net
+{
+  public class RaygunClient
+  {
+    private readonly string _apiKey;
+
+    public RaygunClient (string apiKey)
+    {
+      _apiKey = apiKey;
+    }
+
+    private static RaygunClient _client;
+
+    /// <summary>
+    /// Gets the <see cref="RaygunClient"/> created by the Attach method.
+    /// </summary>
+    public static RaygunClient Current
+    {
+      get { return _client; }
+    }
+
+    /// <summary>
+    /// Causes Raygun to listen to and send all unhandled exceptions and unobserved task exceptions.
+    /// </summary>
+    /// <param name="apiKey">Your app api key.</param>
+    public static void Attach(string apiKey)
+    {
+      Detach();
+      _client = new RaygunClient(apiKey);
+      AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+      TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
+    }
+
+    /// <summary>
+    /// Detaches Raygun from listening to unhandled exceptions and unobserved task exceptions.
+    /// </summary>
+    public static void Detach()
+    {
+      AppDomain.CurrentDomain.UnhandledException -= CurrentDomain_UnhandledException;
+      TaskScheduler.UnobservedTaskException -= TaskScheduler_UnobservedTaskException;
+    }
+
+    private static void TaskScheduler_UnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+    {
+      if (e.Exception != null)
+      {
+        //_client.Send(e.Exception);
+      }
+    }
+
+    private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+    {
+      if (e.ExceptionObject is Exception)
+      {
+        //_client.Send(e.ExceptionObject as Exception);
+      }
+    }
+  }
+}
+
