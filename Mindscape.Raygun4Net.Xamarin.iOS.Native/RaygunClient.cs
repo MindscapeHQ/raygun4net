3a0d6f9cf6c9037788545b6c24ab98621bddecc0
Martin Holman <martin308@gmail.com>
2014-08-11 15:44:22 +1200

    enable crash reporting method

---
+++ Mindscape.Raygun4Net.Xamarin.iOS.Native/RaygunClient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Mindscape.Raygun4Net.Xamarin.iOS.Native
+{
+  public static class RaygunClient
+  {
+    [DllImport ("libc")]
+    private static extern int sigaction (Signal sig, IntPtr act, IntPtr oact);
+
+    enum Signal {
+      SIGBUS = 10,
+      SIGSEGV = 11
+    }
+
+    public static void EnableCrashReporting(string apiKey)  {
+
+      IntPtr sigbus = Marshal.AllocHGlobal (512);
+      IntPtr sigsegv = Marshal.AllocHGlobal (512);
+
+      // Store Mono SIGSEGV and SIGBUS handlers
+      sigaction (Signal.SIGBUS, IntPtr.Zero, sigbus);
+      sigaction (Signal.SIGSEGV, IntPtr.Zero, sigsegv);
+
+      Mindscape.Raygun4Net.Xamarin.iOS.Native.Raygun.SharedReporterWithApiKey (apiKey);
+
+      // Restore Mono SIGSEGV and SIGBUS handlers
+      sigaction (Signal.SIGBUS, sigbus, IntPtr.Zero);
+      sigaction (Signal.SIGSEGV, sigsegv, IntPtr.Zero);
+    }
+  }
+}
+
