aa28c094cb6222fce93a5da0cb36eb3c282578b4
Jason Fauchelle <jason@mindscape.co.nz>
2014-09-22 16:12:18 +1200

    Raygun4Net4

---
+++ Mindscape.Raygun4Net4/Messages/RaygunClientMessage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Mindscape.Raygun4Net.Messages
+{
+  public class RaygunClientMessage
+  {
+    public RaygunClientMessage()
+    {
+      Name = "Raygun4Net4.0"; // 4 is the version of .Net, not the Raygun provider.
+      Version = Assembly.GetAssembly(typeof(RaygunClient)).GetName().Version.ToString();
+      ClientUrl = @"https://github.com/MindscapeHQ/raygun4net";
+    }
+
+    public string Name { get; set; }
+
+    public string Version { get; set; }
+
+    public string ClientUrl { get; set; }
+  }
+}
