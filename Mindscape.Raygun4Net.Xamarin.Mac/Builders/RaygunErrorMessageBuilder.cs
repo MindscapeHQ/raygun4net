086534fe8fdb9eae9f35e05eb20dbbbf062c9509
QuantumNightmare <quantumnightmare@hotmail.com>
2014-10-08 12:10:45 +1300

    Removed duplicate method.

--- Mindscape.Raygun4Net.Xamarin.Mac/Builders/RaygunErrorMessageBuilder.cs
+++ Mindscape.Raygun4Net.Xamarin.Mac/Builders/RaygunErrorMessageBuilder.cs
@@ -7,7 +7,7 @@ using System.Diagnostics;
 
 namespace Mindscape.Raygun4Net.Builders
 {
-  public class RaygunErrorMessageBuilder
+  public class RaygunErrorMessageBuilder : RaygunErrorMessageBuilderBase
   {
     public RaygunErrorMessage Build(Exception exception)
     {
@@ -80,9 +80,7 @@ namespace Mindscape.Raygun4Net.Builders
 
           string file = frame.GetFileName();
 
-          string className = method.ReflectedType != null
-            ? method.ReflectedType.FullName
-            : "(unknown)";
+          string className = method.ReflectedType != null ? method.ReflectedType.FullName : "(unknown)";
 
           var line = new RaygunErrorStackTraceLineMessage
           {
@@ -187,47 +185,6 @@ namespace Mindscape.Raygun4Net.Builders
       }
 
       return lines.ToArray();
-    }
-
-    private string GenerateMethodName(MethodBase method)
-    {
-      var stringBuilder = new StringBuilder();
-
-      stringBuilder.Append(method.Name);
-
-      if (method is MethodInfo && method.IsGenericMethod)
-      {
-        Type[] genericArguments = method.GetGenericArguments();
-        stringBuilder.Append("[");
-        int index2 = 0;
-        bool flag2 = true;
-        for (; index2 < genericArguments.Length; ++index2)
-        {
-          if (!flag2)
-            stringBuilder.Append(",");
-          else
-            flag2 = false;
-          stringBuilder.Append(genericArguments[index2].Name);
-        }
-        stringBuilder.Append("]");
-      }
-      stringBuilder.Append("(");
-      ParameterInfo[] parameters = method.GetParameters();
-      bool flag3 = true;
-      for (int index2 = 0; index2 < parameters.Length; ++index2)
-      {
-        if (!flag3)
-          stringBuilder.Append(", ");
-        else
-          flag3 = false;
-        string str2 = "<UnknownType>";
-        if (parameters[index2].ParameterType != null)
-          str2 = parameters[index2].ParameterType.Name;
-        stringBuilder.Append(str2 + " " + parameters[index2].Name);
-      }
-      stringBuilder.Append(")");
-
-      return stringBuilder.ToString();
     }
   }
 }
