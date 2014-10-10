725b33ae75fc1824b5d1214519895eaf11802011
Jason Fauchelle <jason@mindscape.co.nz>
2014-10-10 14:04:40 +1300

    Move environment message logic into a builder class.

---
+++ Mindscape.Raygun4Net/Builders/RaygunEnvironmentMessageBuilder.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Security;
+using System.Security.Permissions;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.VisualBasic.Devices;
+using Mindscape.Raygun4Net.Messages;
+
+namespace Mindscape.Raygun4Net.Builders
+{
+  public class RaygunEnvironmentMessageBuilder
+  {
+    public static RaygunEnvironmentMessage Build()
+    {
+      RaygunEnvironmentMessage message = new RaygunEnvironmentMessage();
+
+      message.WindowBoundsWidth = SystemInformation.VirtualScreen.Width;
+      message.WindowBoundsHeight = SystemInformation.VirtualScreen.Height;
+      ComputerInfo info = new ComputerInfo();
+      message.Locale = CultureInfo.CurrentCulture.DisplayName;
+
+      DateTime now = DateTime.Now;
+      message.UtcOffset = TimeZone.CurrentTimeZone.GetUtcOffset(now).TotalHours;
+
+      message.OSVersion = info.OSVersion;
+
+      bool mediumTrust = RaygunSettings.Settings.MediumTrust || !HasUnrestrictedFeatureSet;
+
+      if (!mediumTrust)
+      {
+        try
+        {
+          // ProcessorCount cannot run in medium trust under net35, once we have
+          // moved to net40 minimum we can move this out of here
+          message.ProcessorCount = Environment.ProcessorCount;
+          message.Architecture = Environment.GetEnvironmentVariable("PROCESSOR_ARCHITECTURE");
+          message.TotalPhysicalMemory = (ulong)info.TotalPhysicalMemory / 0x100000; // in MB
+          message.AvailablePhysicalMemory = (ulong)info.AvailablePhysicalMemory / 0x100000;
+          message.TotalVirtualMemory = info.TotalVirtualMemory / 0x100000;
+          message.AvailableVirtualMemory = info.AvailableVirtualMemory / 0x100000;
+          message.DiskSpaceFree = GetDiskSpace();
+          message.Cpu = GetCpu();
+          message.OSVersion = GetOSVersion();
+        }
+        catch (SecurityException)
+        {
+          System.Diagnostics.Trace.WriteLine("RaygunClient error: couldn't access environment variables. If you are running in Medium Trust, in web.config in RaygunSettings set mediumtrust=\"true\"");
+        }
+      }
+
+      return message;
+    }
+
+    private static string GetCpu()
+    {
+      ManagementObjectSearcher wmiProcessorSearcher = new ManagementObjectSearcher("SELECT Name FROM Win32_Processor");
+      ManagementObjectCollection wmiProcessorCollection = wmiProcessorSearcher.Get();
+
+      foreach (ManagementObject wmiProcessorObject in wmiProcessorCollection)
+      {
+        try
+        {
+          var name = wmiProcessorObject["Name"].ToString();
+          return name;
+        }
+        catch (ManagementException)
+        {
+        }
+      }
+      return Environment.GetEnvironmentVariable("PROCESSOR_IDENTIFIER");
+    }
+
+    private static string GetOSVersion()
+    {
+      ManagementObjectSearcher wmiOperatingSystemSearcher = new ManagementObjectSearcher("SELECT Version FROM Win32_OperatingSystem");
+      ManagementObjectCollection wmiOperatingSystemCollection = wmiOperatingSystemSearcher.Get();
+
+      foreach (ManagementObject wmiOperatingSystemObject in wmiOperatingSystemCollection)
+      {
+        try
+        {
+          var version = wmiOperatingSystemObject["Version"].ToString();
+          return version;
+        }
+        catch (ManagementException)
+        {
+        }
+      }
+      return Environment.OSVersion.Version.ToString(3);
+    }
+
+    private static List<double> GetDiskSpace()
+    {
+      List<double> diskSpaceFree = new List<double>();
+      foreach (DriveInfo drive in DriveInfo.GetDrives())
+      {
+        if (drive.IsReady)
+        {
+          diskSpaceFree.Add((double)drive.AvailableFreeSpace / 0x40000000); // in GB
+        }
+      }
+      return diskSpaceFree;
+    }
+
+    private static volatile bool _unrestrictedFeatureSet = false;
+    private static volatile bool _determinedUnrestrictedFeatureSet = false;
+    private static readonly object _threadLock = new object();
+
+    private static bool HasUnrestrictedFeatureSet
+    {
+      get
+      {
+        if (!_determinedUnrestrictedFeatureSet)
+        {
+          lock (_threadLock)
+          {
+            if (!_determinedUnrestrictedFeatureSet)
+            {
+              // This seems to crash if not in full trust:
+              //_unrestrictedFeatureSet = AppDomain.CurrentDomain.ApplicationTrust == null;// || AppDomain.CurrentDomain.ApplicationTrust.DefaultGrantSet.PermissionSet.IsUnrestricted();
+              try
+              {
+                // See if we're running in full trust:
+                new PermissionSet(PermissionState.Unrestricted).Demand();
+                _unrestrictedFeatureSet = true;
+              }
+              catch (SecurityException)
+              {
+                _unrestrictedFeatureSet = false;
+              }
+
+              _determinedUnrestrictedFeatureSet = true;
+            }
+          }
+        }
+        return _unrestrictedFeatureSet;
+      }
+    }
+  }
+}
