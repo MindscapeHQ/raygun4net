ed111f6e1e6f1b063fda0e19a618c94bfd55d70f
Jason Fauchelle <quantumnightmare@hotmail.com>
2014-10-10 14:36:32 +1300

    Separated Environment build logic for WinRT and WindowsStore

---
+++ Mindscape.Raygun4Net.WinRT/Builders/RaygunEnvironmentMessageBuilder.cs
@@ -0,0 +1,38 @@
+﻿using Mindscape.Raygun4Net.Messages;
+using System;
+using Windows.ApplicationModel;
+using Windows.Graphics.Display;
+using Windows.UI.ViewManagement;
+
+namespace Mindscape.Raygun4Net.Builders
+{
+  public class RaygunEnvironmentMessageBuilder
+  {
+    public static RaygunEnvironmentMessage Build()
+    {
+      RaygunEnvironmentMessage message = new RaygunEnvironmentMessage();
+
+      if (Windows.UI.Xaml.Window.Current != null)
+      {
+        message.WindowBoundsHeight = Windows.UI.Xaml.Window.Current.Bounds.Height;
+        message.WindowBoundsWidth = Windows.UI.Xaml.Window.Current.Bounds.Width;
+      }
+      message.PackageVersion = string.Format("{0}.{1}", Package.Current.Id.Version.Major, Package.Current.Id.Version.Minor);
+      message.Cpu = Package.Current.Id.Architecture.ToString();
+      message.ResolutionScale = DisplayProperties.ResolutionScale.ToString();
+      message.CurrentOrientation = DisplayProperties.CurrentOrientation.ToString();
+      message.Locale = Windows.System.UserProfile.GlobalizationPreferences.HomeGeographicRegion;
+
+      DateTime now = DateTime.Now;
+      message.UtcOffset = TimeZoneInfo.Local.GetUtcOffset(now).TotalHours;
+
+      SYSTEM_INFO systemInfo = new SYSTEM_INFO();
+      RaygunSystemInfoWrapper.GetNativeSystemInfo(ref systemInfo);
+      message.Architecture = ((PROCESSOR_ARCHITECTURE)systemInfo.wProcessorArchitecture).ToString();
+      message.ProcessorCount = (int)systemInfo.dwNumberOfProcessors;
+      message.ViewState = ApplicationView.Value.ToString();
+
+      return message;
+    }
+  }
+}
---
+++ Mindscape.Raygun4Net.WindowsStore/Builders/RaygunEnvironmentMessageBuilder.cs
@@ -0,0 +1,54 @@
+﻿using Mindscape.Raygun4Net.Messages;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Security.ExchangeActiveSyncProvisioning;
+using Windows.UI.Xaml;
+
+namespace Mindscape.Raygun4Net.Builders
+{
+  public class RaygunEnvironmentMessageBuilder
+  {
+    public static RaygunEnvironmentMessage Build()
+    {
+      RaygunEnvironmentMessage message = new RaygunEnvironmentMessage();
+
+      message.Locale = CultureInfo.CurrentCulture.DisplayName;
+
+      DateTime now = DateTime.Now;
+      message.UtcOffset = TimeZoneInfo.Local.GetUtcOffset(now).TotalHours;
+
+      if (Window.Current != null)
+      {
+        message.WindowBoundsWidth = Window.Current.Bounds.Width;
+        message.WindowBoundsHeight = Window.Current.Bounds.Height;
+
+        var sensor = Windows.Devices.Sensors.SimpleOrientationSensor.GetDefault();
+
+        if (sensor != null)
+        {
+          message.CurrentOrientation = sensor.GetCurrentOrientation().ToString();
+        }
+      }
+
+      var deviceInfo = new EasClientDeviceInformation();
+
+      try
+      {
+        message.DeviceManufacturer = deviceInfo.SystemManufacturer;
+        message.DeviceName = deviceInfo.SystemProductName;
+        message.OSVersion = deviceInfo.OperatingSystem;
+      }
+      catch (Exception e)
+      {
+        Debug.WriteLine("Failed to get device information: {0}", e.Message);
+      }
+
+      return message;
+    }
+  }
+}
