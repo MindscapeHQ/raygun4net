3d5435d6b826a5f9a2d07b6272a507a54caa66b6
Jason Fauchelle <jason@mindscape.co.nz>
2014-10-29 16:05:03 +1300

    Unit tests

---
+++ Mindscape.Raygun4Net.Tests/Model/FakeException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mindscape.Raygun4Net.Tests
+{
+  public class FakeException : Exception
+  {
+    private IDictionary _data;
+
+    public FakeException(IDictionary data)
+    {
+      _data = data;
+    }
+
+    public override System.Collections.IDictionary Data
+    {
+      get
+      {
+        return _data;
+      }
+    }
+  }
+}
---
+++ Mindscape.Raygun4Net.Tests/RaygunClientBaseTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Mindscape.Raygun4Net.Tests
+{
+  [TestFixture]
+  public class RaygunClientBaseTests
+  {
+    private FakeRaygunClient _client = new FakeRaygunClient();
+
+    [Test]
+    public void FlagAsSentDoesNotCrash_DataDoesNotSupportStringKeys()
+    {
+      Assert.That(() => _client.ExposeFlagAsSent(new FakeException(new Dictionary<int, object>())), Throws.Nothing);
+    }
+
+    [Test]
+    public void FlagAsSentDoesNotCrash_NullData()
+    {
+      Assert.That(() => _client.ExposeFlagAsSent(new FakeException(null)), Throws.Nothing);
+    }
+
+    [Test]
+    public void CanSendIfDataIsNull()
+    {
+      Assert.IsTrue(_client.ExposeCanSend(new FakeException(null)));
+    }
+  }
+}
