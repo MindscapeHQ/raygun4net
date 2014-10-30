18ce7cf5611a7105cbbf44ee397dabc42f41b5e5
Jason Fauchelle <jason@mindscape.co.nz>
2014-10-30 13:01:13 +1300

    Moved new SendingMessage logic to base class to be shared by more providers.

--- Mindscape.Raygun4Net/RaygunClientBase.cs
+++ Mindscape.Raygun4Net/RaygunClientBase.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using Mindscape.Raygun4Net.Messages;
 
 namespace Mindscape.Raygun4Net
 {
-  public class RaygunClientBase
+  public abstract class RaygunClientBase
   {
     protected internal const string SentKey = "AlreadySentByRaygun";
 
@@ -32,5 +30,48 @@ namespace Mindscape.Raygun4Net
         }
       }
     }
+
+    /// <summary>
+    /// Raised just before a message is sent. This can be used to make final adjustments to the <see cref="RaygunMessage"/>, or to cancel the send.
+    /// </summary>
+    public event EventHandler<RaygunSendingMessageEventArgs> SendingMessage;
+
+    private bool _handlingRecursiveErrorSending;
+
+    // Returns true if the message can be sent, false if the sending is canceled.
+    protected bool OnSendingMessage(RaygunMessage raygunMessage)
+    {
+      bool result = true;
+
+      if (!_handlingRecursiveErrorSending)
+      {
+        EventHandler<RaygunSendingMessageEventArgs> handler = SendingMessage;
+        if (handler != null)
+        {
+          RaygunSendingMessageEventArgs args = new RaygunSendingMessageEventArgs(raygunMessage);
+          try
+          {
+            handler(this, args);
+          }
+          catch (Exception e)
+          {
+            // Catch and send exceptions that occur in the SendingMessage event handler.
+            // Set the _handlingRecursiveErrorSending flag to prevent infinite errors.
+            _handlingRecursiveErrorSending = true;
+            Send(e);
+            _handlingRecursiveErrorSending = false;
+          }
+          result = !args.Cancel;
+        }
+      }
+
+      return result;
+    }
+
+    /// <summary>
+    /// Transmits an exception to Raygun.io synchronously.
+    /// </summary>
+    /// <param name="exception">The exception to deliver.</param>
+    public abstract void Send(Exception exception);
   }
 }
