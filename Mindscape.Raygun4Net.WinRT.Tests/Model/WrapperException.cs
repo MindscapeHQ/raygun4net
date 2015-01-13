dbabeaa86cc235be25f58cff3986024938bbee73
Jason Fauchelle <quantumnightmare@hotmail.com>
2015-01-13 15:03:18 +1300

    WinRT unit tests

---
+++ Mindscape.Raygun4Net.WinRT.Tests/Model/WrapperException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mindscape.Raygun4Net.WinRT.Tests
+{
+  public class WrapperException : Exception
+  {
+    public WrapperException(Exception innerException)
+      : base("Something went wrong", innerException)
+    {
+    }
+  }
+}
