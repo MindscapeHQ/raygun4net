4f75dec66ceb1debea643e3b55629c82e965b138
Jason Fauchelle <jason@mindscape.co.nz>
2015-03-04 11:25:00 +1300

    Package script update to group all assemblies into folders.

--- package.ps1
+++ package.ps1
@@ -49,53 +49,73 @@ task Zip -depends Package {
     $outerfolder = $release_dir + $version
     $versionfolder = $outerfolder + "\" + $version
     $versionfolder2 = $versionfolder + "\Net2"
+    $versionfolder3 = $versionfolder + "\Net3"
     $versionfolder3clientprofile = $versionfolder + "\Net3.ClientProfile"
     $versionfolder4 = $versionfolder + "\Net4"
     $versionfolder4clientprofile = $versionfolder + "\Net4.ClientProfile"
     $versionfoldermvc = $versionfolder + "\Mvc"
     $versionfolderwebapi = $versionfolder + "\WebApi"
+    $versionfolderwindowsphone = $versionfolder + "\WindowsPhone"
+    $versionfolderwindowsstore = $versionfolder + "\WindowsStore"
+    $versionfolderwinrt = $versionfolder + "\WinRT"
+    $versionfolderxamarinandroid = $versionfolder + "\Xamarin.Android"
+    $versionfolderxamarinios = $versionfolder + "\Xamarin.iOS"
+    $versionfolderxamarinmac = $versionfolder + "\Xamarin.Mac"
     
     $signedfolder = $versionfolder + "\signed"
     $signedfolder2 = $signedfolder + "\Net2"
+    $signedfolder3 = $signedfolder + "\Net3"
     $signedfolder3clientprofile = $signedfolder + "\Net3.ClientProfile"
     $signedfolder4 = $signedfolder + "\Net4"
     $signedfolder4clientprofile = $signedfolder + "\Net4.ClientProfile"
+    $signedfolderwindowsstore = $signedfolder + "\WindowsStore"
+    $signedfolderwinrt = $signedfolder + "\WinRT"
     
     new-item $versionfolder -itemType directory | Out-Null
     new-item $versionfolder2 -itemType directory | Out-Null
+    new-item $versionfolder3 -itemType directory | Out-Null
     new-item $versionfolder3clientprofile -itemType directory | Out-Null
     new-item $versionfolder4 -itemType directory | Out-Null
     new-item $versionfolder4clientprofile -itemType directory | Out-Null
     new-item $versionfoldermvc -itemType directory | Out-Null
     new-item $versionfolderwebapi -itemType directory | Out-Null
+    new-item $versionfolderwindowsphone -itemType directory | Out-Null
+    new-item $versionfolderwindowsstore -itemType directory | Out-Null
+    new-item $versionfolderwinrt -itemType directory | Out-Null
+    new-item $versionfolderxamarinandroid -itemType directory | Out-Null
+    new-item $versionfolderxamarinios -itemType directory | Out-Null
+    new-item $versionfolderxamarinmac -itemType directory | Out-Null
     
     new-item $signedfolder -itemType directory | Out-Null
     new-item $signedfolder2 -itemType directory | Out-Null
+    new-item $signedfolder3 -itemType directory | Out-Null
     new-item $signedfolder3clientprofile -itemType directory | Out-Null
     new-item $signedfolder4 -itemType directory | Out-Null
     new-item $signedfolder4clientprofile -itemType directory | Out-Null
+    new-item $signedfolderwindowsstore -itemType directory | Out-Null
+    new-item $signedfolderwinrt -itemType directory | Out-Null
   
     # .Net 3.5
-    copy-item $build_dir/Mindscape.Raygun4Net.dll $versionfolder
-    copy-item $build_dir/Mindscape.Raygun4Net.pdb $versionfolder
+    copy-item $build_dir/Mindscape.Raygun4Net.dll $versionfolder3
+    copy-item $build_dir/Mindscape.Raygun4Net.pdb $versionfolder3
     # Windows Phone
-    copy-item $build_dir/Mindscape.Raygun4Net.WindowsPhone.dll $versionfolder
-    copy-item $build_dir/Mindscape.Raygun4Net.WindowsPhone.pdb $versionfolder
+    copy-item $build_dir/Mindscape.Raygun4Net.WindowsPhone.dll $versionfolderwindowsphone
+    copy-item $build_dir/Mindscape.Raygun4Net.WindowsPhone.pdb $versionfolderwindowsphone
     # WinRT
-    copy-item $build_dir/Mindscape.Raygun4Net.WinRT.dll $versionfolder
-    copy-item $build_dir/Mindscape.Raygun4Net.WinRT.pdb $versionfolder
+    copy-item $build_dir/Mindscape.Raygun4Net.WinRT.dll $versionfolderwinrt
+    copy-item $build_dir/Mindscape.Raygun4Net.WinRT.pdb $versionfolderwinrt
     # Xamarin.Android
-    copy-item $build_dir/Mindscape.Raygun4Net.Xamarin.Android.dll $versionfolder
-    copy-item $build_dir/Mindscape.Raygun4Net.Xamarin.Android.pdb $versionfolder
+    copy-item $build_dir/Mindscape.Raygun4Net.Xamarin.Android.dll $versionfolderxamarinandroid
+    copy-item $build_dir/Mindscape.Raygun4Net.Xamarin.Android.pdb $versionfolderxamarinandroid
     # Xamarin.iOS
-    copy-item $build_dir/Mindscape.Raygun4Net.Xamarin.iOS.dll $versionfolder
-    copy-item $build_dir/Mindscape.Raygun4Net.Xamarin.iOS.Unified.dll $versionfolder
+    copy-item $build_dir/Mindscape.Raygun4Net.Xamarin.iOS.dll $versionfolderxamarinios
+    copy-item $build_dir/Mindscape.Raygun4Net.Xamarin.iOS.Unified.dll $versionfolderxamarinios
     # Xamarin.Mac
-    copy-item $build_dir/Mindscape.Raygun4Net.Xamarin.Mac.dll $versionfolder
-    copy-item $build_dir/Mindscape.Raygun4Net.Xamarin.Mac.Unified.dll $versionfolder
+    copy-item $build_dir/Mindscape.Raygun4Net.Xamarin.Mac.dll $versionfolderxamarinmac
+    copy-item $build_dir/Mindscape.Raygun4Net.Xamarin.Mac.Unified.dll $versionfolderxamarinmac
     # Windows Store
-    copy-item $build_dir/Mindscape.Raygun4Net.WindowsStore.dll $versionfolder
-    copy-item $build_dir/Mindscape.Raygun4Net.WindowsStore.pdb $versionfolder
+    copy-item $build_dir/Mindscape.Raygun4Net.WindowsStore.dll $versionfolderwindowsstore
+    copy-item $build_dir/Mindscape.Raygun4Net.WindowsStore.pdb $versionfolderwindowsstore
     # .Net 2.0
     copy-item $build_dir2/Mindscape.Raygun4Net.dll $versionfolder2
     copy-item $build_dir2/Mindscape.Raygun4Net.pdb $versionfolder2
@@ -103,28 +123,30 @@ task Zip -depends Package {
     copy-item $build_dir3_client_profile/Mindscape.Raygun4Net.dll $versionfolder3clientprofile
     copy-item $build_dir3_client_profile/Mindscape.Raygun4Net.pdb $versionfolder3clientprofile
     # .Net 4.0
-    copy-item $build_dir4/Mindscape.Raygun4Net.dll $versionfolder4
-    copy-item $build_dir4/Mindscape.Raygun4Net.pdb $versionfolder4
+    copy-item $build_dir4/Mindscape.Raygun4Net4.dll $versionfolder4
+    copy-item $build_dir4/Mindscape.Raygun4Net4.pdb $versionfolder4
     # .Net 4.0 Client Profile
     copy-item $build_dir4_client_profile/Mindscape.Raygun4Net.dll $versionfolder4clientprofile
     copy-item $build_dir4_client_profile/Mindscape.Raygun4Net.pdb $versionfolder4clientprofile
     # .Net MVC
-    copy-item $build_dir_mvc/Mindscape.Raygun4Net.Mvc.dll $versionfoldermvc
-    copy-item $build_dir_mvc/Mindscape.Raygun4Net.Mvc.pdb $versionfoldermvc
     copy-item $build_dir_mvc/Mindscape.Raygun4Net.dll $versionfoldermvc
     copy-item $build_dir_mvc/Mindscape.Raygun4Net.pdb $versionfoldermvc
+    copy-item $build_dir_mvc/Mindscape.Raygun4Net.Mvc.dll $versionfoldermvc
+    copy-item $build_dir_mvc/Mindscape.Raygun4Net.Mvc.pdb $versionfoldermvc
+    copy-item $build_dir_mvc/Mindscape.Raygun4Net4.dll $versionfoldermvc
+    copy-item $build_dir_mvc/Mindscape.Raygun4Net4.pdb $versionfoldermvc
     # .Net WebApi
     copy-item $build_dir_webapi/Mindscape.Raygun4Net.WebApi.dll $versionfolderwebapi
     copy-item $build_dir_webapi/Mindscape.Raygun4Net.WebApi.pdb $versionfolderwebapi
     copy-item $build_dir_webapi/Mindscape.Raygun4Net.dll $versionfolderwebapi
     copy-item $build_dir_webapi/Mindscape.Raygun4Net.pdb $versionfolderwebapi
     # Signed
-    copy-item $signed_build_dir/Mindscape.Raygun4Net.dll $signedfolder
-    copy-item $signed_build_dir/Mindscape.Raygun4Net.WinRT.dll $signedfolder
-    copy-item $signed_build_dir/Mindscape.Raygun4Net.WindowsStore.dll $signedfolder
+    copy-item $signed_build_dir/Mindscape.Raygun4Net.dll $signedfolder3
+    copy-item $signed_build_dir/Mindscape.Raygun4Net.WinRT.dll $signedfolderwinrt
+    copy-item $signed_build_dir/Mindscape.Raygun4Net.WindowsStore.dll $signedfolderwindowsstore
     copy-item $signed_build_dir2/Mindscape.Raygun4Net.dll $signedfolder2
     copy-item $signed_build_dir3_client_profile/Mindscape.Raygun4Net.dll $signedfolder3clientprofile
-    copy-item $signed_build_dir4/Mindscape.Raygun4Net.dll $signedfolder4
+    copy-item $signed_build_dir4/Mindscape.Raygun4Net4.dll $signedfolder4
     copy-item $signed_build_dir4_client_profile/Mindscape.Raygun4Net.dll $signedfolder4clientprofile
 	
     $zipFullName = $release_dir + $version + ".zip"
