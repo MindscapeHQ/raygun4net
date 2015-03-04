c35397f80c7f4d8c8e0c77166dcda831ab8e5879
Jason Fauchelle <jason@mindscape.co.nz>
2015-03-05 10:53:28 +1300

    Cleaned up the unit test project.

--- Mindscape.Raygun4Net.ClientProfile.Tests/RaygunClientTests.cs
+++ Mindscape.Raygun4Net.ClientProfile.Tests/RaygunClientTests.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Net;
 using System.Reflection;
-using System.Web;
 using Mindscape.Raygun4Net.Messages;
 using NUnit.Framework;
 
