1f0c50169b55b34451d96850b6a3d9e9c0c4a3b8
Jason Fauchelle <jason@mindscape.co.nz>
2015-03-04 17:00:57 +1300

    Quick POC for .Net 4.0 client profile

---
+++ Mindscape.Raygun4Net4.ClientProfile/RaygunClient.cs
@@ -0,0 +1,455 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using Mindscape.Raygun4Net.Messages;
+
+using System.Web;
+using System.Threading;
+using System.Reflection;
+using Mindscape.Raygun4Net.Builders;
+
+namespace Mindscape.Raygun4Net
+{
+  public class RaygunClient : RaygunClientBase
+  {
+    private readonly string _apiKey;
+    private readonly RaygunRequestMessageOptions _requestMessageOptions = new RaygunRequestMessageOptions();
+    private readonly List<Type> _wrapperExceptions = new List<Type>();
+
+    //[ThreadStatic]
+    //private static RaygunRequestMessage _currentRequestMessage;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RaygunClient" /> class.
+    /// </summary>
+    /// <param name="apiKey">The API key.</param>
+    public RaygunClient(string apiKey)
+    {
+      _apiKey = apiKey;
+
+      _wrapperExceptions.Add(typeof(TargetInvocationException));
+
+      if (!string.IsNullOrEmpty(RaygunSettings.Settings.IgnoreFormFieldNames))
+      {
+        var ignoredNames = RaygunSettings.Settings.IgnoreFormFieldNames.Split(',');
+        IgnoreFormFieldNames(ignoredNames);
+      }
+      if (!string.IsNullOrEmpty(RaygunSettings.Settings.IgnoreHeaderNames))
+      {
+        var ignoredNames = RaygunSettings.Settings.IgnoreHeaderNames.Split(',');
+        IgnoreHeaderNames(ignoredNames);
+      }
+      if (!string.IsNullOrEmpty(RaygunSettings.Settings.IgnoreCookieNames))
+      {
+        var ignoredNames = RaygunSettings.Settings.IgnoreCookieNames.Split(',');
+        IgnoreCookieNames(ignoredNames);
+      }
+      if (!string.IsNullOrEmpty(RaygunSettings.Settings.IgnoreServerVariableNames))
+      {
+        var ignoredNames = RaygunSettings.Settings.IgnoreServerVariableNames.Split(',');
+        IgnoreServerVariableNames(ignoredNames);
+      }
+      IsRawDataIgnored = RaygunSettings.Settings.IsRawDataIgnored;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RaygunClient" /> class.
+    /// Uses the ApiKey specified in the config file.
+    /// </summary>
+    public RaygunClient()
+      : this(RaygunSettings.Settings.ApiKey)
+    {
+    }
+
+    protected bool ValidateApiKey()
+    {
+      if (string.IsNullOrEmpty(_apiKey))
+      {
+        System.Diagnostics.Debug.WriteLine("ApiKey has not been provided, exception will not be logged");
+        return false;
+      }
+      return true;
+    }
+
+    /// <summary>
+    /// Gets or sets the username/password credentials which are used to authenticate with the system default Proxy server, if one is set
+    /// and requires credentials.
+    /// </summary>
+    public ICredentials ProxyCredentials { get; set; }
+
+    /// <summary>
+    /// Gets or sets an IWebProxy instance which can be used to override the default system proxy server settings
+    /// </summary>
+    public IWebProxy WebProxy { get; set; }
+
+    /// <summary>
+    /// Adds a list of outer exceptions that will be stripped, leaving only the valuable inner exception.
+    /// This can be used when a wrapper exception, e.g. TargetInvocationException or HttpUnhandledException,
+    /// contains the actual exception as the InnerException. The message and stack trace of the inner exception will then
+    /// be used by Raygun for grouping and display. The above two do not need to be added manually,
+    /// but if you have other wrapper exceptions that you want stripped you can pass them in here.
+    /// </summary>
+    /// <param name="wrapperExceptions">Exception types that you want removed and replaced with their inner exception.</param>
+    public void AddWrapperExceptions(params Type[] wrapperExceptions)
+    {
+      foreach (Type wrapper in wrapperExceptions)
+      {
+        if (!_wrapperExceptions.Contains(wrapper))
+        {
+          _wrapperExceptions.Add(wrapper);
+        }
+      }
+    }
+
+    /// <summary>
+    /// Specifies types of wrapper exceptions that Raygun should send rather than stripping out and sending the inner exception.
+    /// This can be used to remove the default wrapper exceptions (TargetInvocationException and HttpUnhandledException).
+    /// </summary>
+    /// <param name="wrapperExceptions">Exception types that should no longer be stripped away.</param>
+    public void RemoveWrapperExceptions(params Type[] wrapperExceptions)
+    {
+      foreach (Type wrapper in wrapperExceptions)
+      {
+        _wrapperExceptions.Remove(wrapper);
+      }
+    }
+
+    /// <summary>
+    /// Adds a list of keys to ignore when attaching the Form data of an HTTP POST request. This allows
+    /// you to remove sensitive data from the transmitted copy of the Form on the HttpRequest by specifying the keys you want removed.
+    /// This method is only effective in a web context.
+    /// </summary>
+    /// <param name="names">Keys to be stripped from the copy of the Form NameValueCollection when sending to Raygun.</param>
+    public void IgnoreFormFieldNames(params string[] names)
+    {
+      _requestMessageOptions.AddFormFieldNames(names);
+    }
+
+    /// <summary>
+    /// Adds a list of keys to ignore when attaching the headers of an HTTP POST request. This allows
+    /// you to remove sensitive data from the transmitted copy of the Headers on the HttpRequest by specifying the keys you want removed.
+    /// This method is only effective in a web context.
+    /// </summary>
+    /// <param name="names">Keys to be stripped from the copy of the Headers NameValueCollection when sending to Raygun.</param>
+    public void IgnoreHeaderNames(params string[] names)
+    {
+      _requestMessageOptions.AddHeaderNames(names);
+    }
+
+    /// <summary>
+    /// Adds a list of keys to ignore when attaching the cookies of an HTTP POST request. This allows
+    /// you to remove sensitive data from the transmitted copy of the Cookies on the HttpRequest by specifying the keys you want removed.
+    /// This method is only effective in a web context.
+    /// </summary>
+    /// <param name="names">Keys to be stripped from the copy of the Cookies NameValueCollection when sending to Raygun.</param>
+    public void IgnoreCookieNames(params string[] names)
+    {
+      _requestMessageOptions.AddCookieNames(names);
+    }
+
+    /// <summary>
+    /// Adds a list of keys to ignore when attaching the server variables of an HTTP POST request. This allows
+    /// you to remove sensitive data from the transmitted copy of the ServerVariables on the HttpRequest by specifying the keys you want removed.
+    /// This method is only effective in a web context.
+    /// </summary>
+    /// <param name="names">Keys to be stripped from the copy of the ServerVariables NameValueCollection when sending to Raygun.</param>
+    public void IgnoreServerVariableNames(params string[] names)
+    {
+      _requestMessageOptions.AddServerVariableNames(names);
+    }
+
+    /// <summary>
+    /// Specifies whether or not RawData from web requests is ignored when sending reports to Raygun.io.
+    /// The default is false which means RawData will be sent to Raygun.io.
+    /// </summary>
+    public bool IsRawDataIgnored
+    {
+      get { return _requestMessageOptions.IsRawDataIgnored; }
+      set
+      {
+        _requestMessageOptions.IsRawDataIgnored = value;
+      }
+    }
+
+    /// <summary>
+    /// Transmits an exception to Raygun.io synchronously, using the version number of the originating assembly.
+    /// </summary>
+    /// <param name="exception">The exception to deliver.</param>
+    public override void Send(Exception exception)
+    {
+      Send(exception, null, (IDictionary)null, null);
+    }
+
+    /// <summary>
+    /// Transmits an exception to Raygun.io synchronously specifying a list of string tags associated
+    /// with the message for identification. This uses the version number of the originating assembly.
+    /// </summary>
+    /// <param name="exception">The exception to deliver.</param>
+    /// <param name="tags">A list of strings associated with the message.</param>
+    public void Send(Exception exception, IList<string> tags)
+    {
+      Send(exception, tags, (IDictionary)null, null);
+    }
+
+    /// <summary>
+    /// Transmits an exception to Raygun.io synchronously specifying a list of string tags associated
+    /// with the message for identification, as well as sending a key-value collection of custom data.
+    /// This uses the version number of the originating assembly.
+    /// </summary>
+    /// <param name="exception">The exception to deliver.</param>
+    /// <param name="tags">A list of strings associated with the message.</param>
+    /// <param name="userCustomData">A key-value collection of custom data that will be added to the payload.</param>
+    public void Send(Exception exception, IList<string> tags, IDictionary userCustomData)
+    {
+      Send(exception, tags, userCustomData, null);
+    }
+
+    /// <summary>
+    /// Transmits an exception to Raygun.io synchronously specifying a list of string tags associated
+    /// with the message for identification, as well as sending a key-value collection of custom data.
+    /// This uses the version number of the originating assembly.
+    /// </summary>
+    /// <param name="exception">The exception to deliver.</param>
+    /// <param name="tags">A list of strings associated with the message.</param>
+    /// <param name="userCustomData">A key-value collection of custom data that will be added to the payload.</param>
+    /// <param name="userInfo">Information about the user including the identity string.</param>
+    public void Send(Exception exception, IList<string> tags, IDictionary userCustomData, RaygunIdentifierMessage userInfo)
+    {
+      if (CanSend(exception))
+      {
+        //_currentRequestMessage = BuildRequestMessage();
+
+        StripAndSend(exception, tags, userCustomData, userInfo);
+        FlagAsSent(exception);
+      }
+    }
+
+    /// <summary>
+    /// Asynchronously transmits a message to Raygun.io.
+    /// </summary>
+    /// <param name="exception">The exception to deliver.</param>
+    public void SendInBackground(Exception exception)
+    {
+      SendInBackground(exception, null, (IDictionary)null, null);
+    }
+
+    /// <summary>
+    /// Asynchronously transmits an exception to Raygun.io.
+    /// </summary>
+    /// <param name="exception">The exception to deliver.</param>
+    /// <param name="tags">A list of strings associated with the message.</param>
+    public void SendInBackground(Exception exception, IList<string> tags)
+    {
+      SendInBackground(exception, tags, (IDictionary)null, null);
+    }
+
+    /// <summary>
+    /// Asynchronously transmits an exception to Raygun.io.
+    /// </summary>
+    /// <param name="exception">The exception to deliver.</param>
+    /// <param name="tags">A list of strings associated with the message.</param>
+    /// <param name="userCustomData">A key-value collection of custom data that will be added to the payload.</param>
+    public void SendInBackground(Exception exception, IList<string> tags, IDictionary userCustomData)
+    {
+      SendInBackground(exception, tags, userCustomData, null);
+    }
+
+    /// <summary>
+    /// Asynchronously transmits an exception to Raygun.io.
+    /// </summary>
+    /// <param name="exception">The exception to deliver.</param>
+    /// <param name="tags">A list of strings associated with the message.</param>
+    /// <param name="userCustomData">A key-value collection of custom data that will be added to the payload.</param>
+    /// <param name="userInfo">Information about the user including the identity string.</param>
+    public void SendInBackground(Exception exception, IList<string> tags, IDictionary userCustomData, RaygunIdentifierMessage userInfo)
+    {
+      if (CanSend(exception))
+      {
+        // We need to process the HttpRequestMessage on the current thread,
+        // otherwise it will be disposed while we are using it on the other thread.
+        //RaygunRequestMessage currentRequestMessage = BuildRequestMessage();
+
+        ThreadPool.QueueUserWorkItem(c =>
+        {
+          try
+          {
+            //_currentRequestMessage = currentRequestMessage;
+            StripAndSend(exception, tags, userCustomData, userInfo);
+          }
+          catch (Exception)
+          {
+            // This will swallow any unhandled exceptions unless we explicitly want to throw on error.
+            // Otherwise this can bring the whole process down.
+            if (RaygunSettings.Settings.ThrowOnError)
+            {
+              throw;
+            }
+          }
+        });
+        FlagAsSent(exception);
+      }
+    }
+
+    /// <summary>
+    /// Asynchronously transmits a message to Raygun.io.
+    /// </summary>
+    /// <param name="raygunMessage">The RaygunMessage to send. This needs its OccurredOn property
+    /// set to a valid DateTime and as much of the Details property as is available.</param>
+    public void SendInBackground(RaygunMessage raygunMessage)
+    {
+      ThreadPool.QueueUserWorkItem(c => Send(raygunMessage));
+    }
+
+    /*private RaygunRequestMessage BuildRequestMessage()
+    {
+      RaygunRequestMessage requestMessage = null;
+      HttpContext context = HttpContext.Current;
+      if (context != null)
+      {
+        HttpRequest request = null;
+        try
+        {
+          request = context.Request;
+        }
+        catch (HttpException ex)
+        {
+          System.Diagnostics.Trace.WriteLine("Error retrieving HttpRequest {0}", ex.Message);
+        }
+
+        if (request != null)
+        {
+          requestMessage = RaygunRequestMessageBuilder.Build(request, _requestMessageOptions);
+        }
+      }
+
+      return requestMessage;
+    }*/
+
+    protected RaygunMessage BuildMessage(Exception exception, IList<string> tags, IDictionary userCustomData)
+    {
+      return BuildMessage(exception, tags, userCustomData, null);
+    }
+
+    protected RaygunMessage BuildMessage(Exception exception, IList<string> tags, IDictionary userCustomData, RaygunIdentifierMessage userInfoMessage)
+    {
+      var message = RaygunMessageBuilder.New
+        //.SetHttpDetails(_currentRequestMessage)
+        .SetEnvironmentDetails()
+        .SetMachineName(Environment.MachineName)
+        .SetExceptionDetails(exception)
+        .SetClientDetails()
+        .SetVersion(ApplicationVersion)
+        .SetTags(tags)
+        .SetUserCustomData(userCustomData)
+        .SetUser(userInfoMessage ?? UserInfo ?? (!String.IsNullOrEmpty(User) ? new RaygunIdentifierMessage(User) : null))
+        .Build();
+      return message;
+    }
+
+    private void StripAndSend(Exception exception, IList<string> tags, IDictionary userCustomData, RaygunIdentifierMessage userInfo)
+    {
+      foreach (Exception e in StripWrapperExceptions(exception))
+      {
+        Send(BuildMessage(e, tags, userCustomData, userInfo));
+      }
+    }
+
+    protected IEnumerable<Exception> StripWrapperExceptions(Exception exception)
+    {
+      if (exception != null && _wrapperExceptions.Any(wrapperException => exception.GetType() == wrapperException && exception.InnerException != null))
+      {
+        AggregateException aggregate = exception as AggregateException;
+        if (aggregate != null)
+        {
+          foreach (Exception e in aggregate.InnerExceptions)
+          {
+            foreach (Exception ex in StripWrapperExceptions(e))
+            {
+              yield return ex;
+            }
+          }
+        }
+        else
+        {
+          foreach (Exception e in StripWrapperExceptions(exception.InnerException))
+          {
+            yield return e;
+          }
+        }
+      }
+      else
+      {
+        yield return exception;
+      }
+    }
+
+    /// <summary>
+    /// Posts a RaygunMessage to the Raygun.io api endpoint.
+    /// </summary>
+    /// <param name="raygunMessage">The RaygunMessage to send. This needs its OccurredOn property
+    /// set to a valid DateTime and as much of the Details property as is available.</param>
+    public void Send(RaygunMessage raygunMessage)
+    {
+      if (ValidateApiKey())
+      {
+        bool canSend = OnSendingMessage(raygunMessage);
+        if (canSend)
+        {
+          using (var client = CreateWebClient())
+          {
+            try
+            {
+              var message = SimpleJson.SerializeObject(raygunMessage);
+              client.UploadString(RaygunSettings.Settings.ApiEndpoint, message);
+            }
+            catch (Exception ex)
+            {
+              System.Diagnostics.Trace.WriteLine(string.Format("Error Logging Exception to Raygun.io {0}", ex.Message));
+
+              if (RaygunSettings.Settings.ThrowOnError)
+              {
+                throw;
+              }
+            }
+          }
+        }
+      }
+    }
+
+    protected WebClient CreateWebClient()
+    {
+      var client = new WebClient();
+      client.Headers.Add("X-ApiKey", _apiKey);
+      client.Headers.Add("content-type", "application/json; charset=utf-8");
+      client.Encoding = System.Text.Encoding.UTF8;
+
+      if (WebProxy != null)
+      {
+        client.Proxy = WebProxy;
+      }
+      else if (WebRequest.DefaultWebProxy != null)
+      {
+        Uri proxyUri = WebRequest.DefaultWebProxy.GetProxy(new Uri(RaygunSettings.Settings.ApiEndpoint.ToString()));
+
+        if (proxyUri != null && proxyUri.AbsoluteUri != RaygunSettings.Settings.ApiEndpoint.ToString())
+        {
+          client.Proxy = new WebProxy(proxyUri, false);
+
+          if (ProxyCredentials == null)
+          {
+            client.UseDefaultCredentials = true;
+            client.Proxy.Credentials = CredentialCache.DefaultCredentials;
+          }
+          else
+          {
+            client.UseDefaultCredentials = false;
+            client.Proxy.Credentials = ProxyCredentials;
+          }
+        }
+      }
+      return client;
+    }
+  }
+}
---
+++ Mindscape.Raygun4Net4.ClientProfile/RaygunMessageBuilder.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Reflection;
+using System.Web;
+using Mindscape.Raygun4Net.Builders;
+using Mindscape.Raygun4Net.Messages;
+
+namespace Mindscape.Raygun4Net
+{
+  public class RaygunMessageBuilder : IRaygunMessageBuilder
+  {
+    public static RaygunMessageBuilder New
+    {
+      get
+      {
+        return new RaygunMessageBuilder();
+      }
+    }
+
+    private readonly RaygunMessage _raygunMessage;
+
+    private RaygunMessageBuilder()
+    {
+      _raygunMessage = new RaygunMessage();
+    }
+
+    public RaygunMessage Build()
+    {
+      return _raygunMessage;
+    }
+
+    public IRaygunMessageBuilder SetMachineName(string machineName)
+    {
+      _raygunMessage.Details.MachineName = machineName;
+      return this;
+    }
+
+    public IRaygunMessageBuilder SetEnvironmentDetails()
+    {
+      _raygunMessage.Details.Environment = RaygunEnvironmentMessageBuilder.Build();
+      return this;
+    }
+
+    public IRaygunMessageBuilder SetExceptionDetails(Exception exception)
+    {
+      if (exception != null)
+      {
+        _raygunMessage.Details.Error = RaygunErrorMessageBuilder.Build(exception);
+      }
+
+      /*HttpException error = exception as HttpException;
+      if (error != null)
+      {
+        int code = error.GetHttpCode();
+        string description = null;
+        if (Enum.IsDefined(typeof(HttpStatusCode), code))
+        {
+          description = ((HttpStatusCode)code).ToString();
+        }
+        _raygunMessage.Details.Response = new RaygunResponseMessage() { StatusCode = code, StatusDescription = description };
+      }*/
+
+      /*WebException webError = exception as WebException;
+      if (webError != null)
+      {
+        if (webError.Status == WebExceptionStatus.ProtocolError && webError.Response is HttpWebResponse)
+        {
+          HttpWebResponse response = (HttpWebResponse)webError.Response;
+          _raygunMessage.Details.Response = new RaygunResponseMessage() { StatusCode = (int)response.StatusCode, StatusDescription = response.StatusDescription };
+        }
+        else if (webError.Status == WebExceptionStatus.ProtocolError && webError.Response is FtpWebResponse)
+        {
+          FtpWebResponse response = (FtpWebResponse)webError.Response;
+          _raygunMessage.Details.Response = new RaygunResponseMessage() { StatusCode = (int)response.StatusCode, StatusDescription = response.StatusDescription };
+        }
+        else
+        {
+          _raygunMessage.Details.Response = new RaygunResponseMessage() { StatusDescription = webError.Status.ToString() };
+        }
+      }*/
+
+      return this;
+    }
+
+    public IRaygunMessageBuilder SetClientDetails()
+    {
+      _raygunMessage.Details.Client = new RaygunClientMessage()
+      {
+        // This is for the MVC project to set the correct client name - due to the client message class being in the core assembly.
+        Name = ((AssemblyTitleAttribute)GetType().Assembly.GetCustomAttributes(typeof(AssemblyTitleAttribute), false)[0]).Title
+      };
+      return this;
+    }
+
+    public IRaygunMessageBuilder SetUserCustomData(IDictionary userCustomData)
+    {
+      _raygunMessage.Details.UserCustomData = userCustomData;
+      return this;
+    }
+
+    public IRaygunMessageBuilder SetTags(IList<string> tags)
+    {
+      _raygunMessage.Details.Tags = tags;
+      return this;
+    }
+
+    public IRaygunMessageBuilder SetUser(RaygunIdentifierMessage user)
+    {
+      _raygunMessage.Details.User = user;
+      return this;
+    }
+
+    /*public IRaygunMessageBuilder SetHttpDetails(HttpContext context, RaygunRequestMessageOptions options = null)
+    {
+      if (context != null)
+      {
+        HttpRequest request;
+        try
+        {
+          request = context.Request;
+        }
+        catch (HttpException)
+        {
+          return this;
+        }
+        _raygunMessage.Details.Request = RaygunRequestMessageBuilder.Build(request, options);
+      }
+
+      return this;
+    }*/
+
+    public IRaygunMessageBuilder SetHttpDetails(RaygunRequestMessage message)
+    {
+      _raygunMessage.Details.Request = message;
+      return this;
+    }
+
+    public IRaygunMessageBuilder SetVersion(string version)
+    {
+      if (!String.IsNullOrEmpty(version))
+      {
+        _raygunMessage.Details.Version = version;
+      }
+      else if (!String.IsNullOrEmpty(RaygunSettings.Settings.ApplicationVersion))
+      {
+        _raygunMessage.Details.Version = RaygunSettings.Settings.ApplicationVersion;
+      }
+      else
+      {
+        var entryAssembly = Assembly.GetEntryAssembly();
+        if (entryAssembly != null)
+        {
+          _raygunMessage.Details.Version = entryAssembly.GetName().Version.ToString();
+        }
+        else
+        {
+          _raygunMessage.Details.Version = "Not supplied";
+        }
+      }
+      return this;
+    }
+  }
+}
