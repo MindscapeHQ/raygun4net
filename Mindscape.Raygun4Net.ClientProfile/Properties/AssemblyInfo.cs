80f08b5cdd02f4b5310e6c5d7a84c2962e9603fa
Jason Fauchelle <jason@mindscape.co.nz>
2015-03-05 09:55:07 +1300

    Updated assembly info.

--- Mindscape.Raygun4Net.ClientProfile/Properties/AssemblyInfo.cs
+++ Mindscape.Raygun4Net.ClientProfile/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Raygun4Net")]
+[assembly: AssemblyTitle("Raygun4Net.ClientProfile")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Mindscape")]
 [assembly: AssemblyProduct("Mindscape Raygun4Net")]
-[assembly: AssemblyCopyright("Copyright © Mindscape 2012-2015")]
+[assembly: AssemblyCopyright("Copyright © Mindscape 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- Mindscape.Raygun4Net4.ClientProfile/Properties/AssemblyInfo.cs
+++ Mindscape.Raygun4Net4.ClientProfile/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Raygun4Net4.0")]
+[assembly: AssemblyTitle("Raygun4Net4.ClientProfile")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Mindscape")]
 [assembly: AssemblyProduct("Mindscape Raygun4Net")]
-[assembly: AssemblyCopyright("Copyright © Mindscape 2014-2015")]
+[assembly: AssemblyCopyright("Copyright © Mindscape 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
