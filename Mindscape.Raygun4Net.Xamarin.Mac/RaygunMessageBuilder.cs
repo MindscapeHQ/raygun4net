44de8bffb5349ee6ffd49a3a4b03578c00862b79
QuantumNightmare <quantumnightmare@hotmail.com>
2015-04-16 14:37:03 +1200

    Implemented SetTimeStamp in iOS and Mac

--- Mindscape.Raygun4Net.Xamarin.Mac/RaygunMessageBuilder.cs
+++ Mindscape.Raygun4Net.Xamarin.Mac/RaygunMessageBuilder.cs
@@ -108,5 +108,14 @@ namespace Mindscape.Raygun4Net
 
       return this;
     }
+
+	public IRaygunMessageBuilder SetTimeStamp(DateTime? currentTime)
+	{
+	  if (currentTime != null)
+	  {
+		_raygunMessage.OccurredOn = currentTime.Value;
+	  }
+	  return this;
+	}
   }
 }
