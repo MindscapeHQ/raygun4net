7f12be765e36a29ae2a071f1f8721a672faaf18e
Jason Fauchelle <jfauchelle@raygun.io>
2015-11-23 13:26:41 +1300

    Don't send exceptions that wrap already sent exceptions

--- Mindscape.Raygun4Net.Xamarin.Android/RaygunClient.cs
+++ Mindscape.Raygun4Net.Xamarin.Android/RaygunClient.cs
@@ -39,7 +39,29 @@ namespace Mindscape.Raygun4Net
       _wrapperExceptions.Add(typeof(TargetInvocationException));
       _wrapperExceptions.Add(typeof(System.AggregateException));
 
+      SendingMessage += RaygunClient_SendingMessage;
+
       ThreadPool.QueueUserWorkItem(state => { SendStoredMessages(); });
+    }
+
+    private void RaygunClient_SendingMessage(object sender, RaygunSendingMessageEventArgs e)
+    {
+      if (e.Message != null && e.Message.Details != null && e.Message.Details.Error != null && e.Message.Details.Error.StackTrace != null && e.Message.Details.Error.StackTrace.Length > 1)
+      {
+        if (e.Message.Details.Error.StackTrace[0].FileName != null && e.Message.Details.Error.StackTrace[0].FileName.Contains("--- End of managed exception stack trace ---"))
+        {
+          foreach (RaygunErrorStackTraceLineMessage line in e.Message.Details.Error.StackTrace)
+          {
+            if (line.FileName != null && line.FileName.Contains("Caused by:") && line.FileName.Contains("JavaProxyThrowable"))
+            {
+              // Reaching this point means the exception is wrapping a managed exception that has already been sent.
+              // Such exception does not contain any additional useful information, and so is a waste to send it.
+              e.Cancel = true;
+              break;
+            }
+          }
+        }
+      }
     }
 
     private bool ValidateApiKey()
