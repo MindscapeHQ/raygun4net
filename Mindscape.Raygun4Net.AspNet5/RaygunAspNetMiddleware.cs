99c0cafc589b22b77659503f726b1a0503a3bc18
Jamie Penney <jpenney@raygun.io>
2016-04-13 11:45:45 +1200

    Finish RaygunAspNetMiddleware

--- Mindscape.Raygun4Net.AspNet5/RaygunAspNetMiddleware.cs
+++ Mindscape.Raygun4Net.AspNet5/RaygunAspNetMiddleware.cs
@@ -8,39 +8,76 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Mindscape.Raygun4Net.AspNet5
 {
+  public interface IRaygunSettingsProvider
+  {
+    RaygunSettings GetRaygunSettings(RaygunSettings baseSettings);
+  }
+
+  public class RaygunSettingsProvider : IRaygunSettingsProvider
+  {
+    private readonly Func<RaygunSettings, RaygunSettings> _customConfig;
+
+    public RaygunSettingsProvider(Func<RaygunSettings, RaygunSettings> customConfig)
+    {
+      this._customConfig = customConfig;
+    }
+
+
+    public RaygunSettings GetRaygunSettings(RaygunSettings baseSettings)
+    {
+      if(_customConfig != null)
+      {
+        return _customConfig(baseSettings);
+      }
+      return baseSettings;
+    }
+  }
+
   public class RaygunAspNetMiddleware
   {
     private readonly RequestDelegate _next;
     private readonly RaygunSettings _settings;
 
-    public RaygunAspNetMiddleware(RequestDelegate next, IOptions<RaygunSettings> settings)
+    public RaygunAspNetMiddleware(RequestDelegate next, IOptions<RaygunSettings> settings, IRaygunSettingsProvider customConfig)
     {
       _next = next;
-      _settings = settings.Value;
-    }
-    public Task Invoke(HttpContext httpContext)
-    {
-      return _next.Invoke(httpContext).ContinueWith(appState =>
-      {
-        if (appState.IsFaulted && appState.Exception != null)
-        {
 
-        }
-      });
+      if(customConfig != null)
+      {
+        _settings = customConfig.GetRaygunSettings(settings.Value ?? new RaygunSettings());  
+      }
+      else
+      {
+        _settings = settings.Value ?? new RaygunSettings();
+      }
+    }
+    public async Task Invoke(HttpContext httpContext)
+    {
+      try
+      {
+        await _next.Invoke(httpContext);
+      }
+      catch(Exception e)
+      {
+        var client = new RaygunAspNet5Client(_settings);
+        await client.SendInBackground(e);
+      }
     }
   }
 
   public static class IApplicationBuilderExtensions
   {
-    public static IApplicationBuilder AddRaygun(this IApplicationBuilder app, Action<RaygunSettings> customConfig = null)
+    public static IApplicationBuilder UseRaygun(this IApplicationBuilder app)
     {
       return app.UseMiddleware<RaygunAspNetMiddleware>();
     }
 
-    public static IServiceCollection LoadRaygunConfiguration(this IServiceCollection services, IConfiguration configuration)
+    public static IServiceCollection AddRaygun(this IServiceCollection services, IConfiguration configuration, Func<RaygunSettings, RaygunSettings> customConfig = null)
     {
       var settings = configuration.GetSection("RaygunSettings");
       services.Configure<RaygunSettings>(settings);
+      services.AddInstance<IRaygunSettingsProvider>(new RaygunSettingsProvider(customConfig));
+      
       return services;
     }
   }
