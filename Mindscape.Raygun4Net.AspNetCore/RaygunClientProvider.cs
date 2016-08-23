3151f65997825d1baebc70725fc07129ddff88f5
Jeremy Norman <jnorman@raygun.io>
2016-08-23 14:35:49 +1200

    Add in the overload for maintaining backwards compatibility

--- Mindscape.Raygun4Net.AspNetCore/RaygunClientProvider.cs
+++ Mindscape.Raygun4Net.AspNetCore/RaygunClientProvider.cs
@@ -4,13 +4,19 @@ namespace Mindscape.Raygun4Net
 {
   public interface IRaygunAspNetCoreClientProvider
   {
-    RaygunClient GetClient(RaygunSettings settings, HttpContext context = null);
+    RaygunClient GetClient(RaygunSettings settings);
+    RaygunClient GetClient(RaygunSettings settings, HttpContext context);
     RaygunSettings GetRaygunSettings(RaygunSettings baseSettings);
   }
 
   public class DefaultRaygunAspNetCoreClientProvider : IRaygunAspNetCoreClientProvider
   {
-    public virtual RaygunClient GetClient(RaygunSettings settings, HttpContext context = null)
+    public virtual RaygunClient GetClient(RaygunSettings settings)
+    {
+      return GetClient(settings, null);
+    }
+
+    public virtual RaygunClient GetClient(RaygunSettings settings, HttpContext context)
     {
       return new RaygunClient(settings, context);
     }
