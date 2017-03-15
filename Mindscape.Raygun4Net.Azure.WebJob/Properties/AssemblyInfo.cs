ec4dec32ba3b17a6f5b5e5b633d8eff0a2af03bd
Taylor Lodge <ubermouse894@gmail.com>
2017-03-15 14:51:12 +1300

    Update AssemblyInfo

--- Mindscape.Raygun4Net.Azure.WebJob/Properties/AssemblyInfo.cs
+++ Mindscape.Raygun4Net.Azure.WebJob/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Mindscape.Raygun4Net.Azure.WebJob")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Mindscape.Raygun4Net.Azure.WebJob")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCompany("Raygun")]
+[assembly: AssemblyProduct("Raygun4Net")]
+[assembly: AssemblyCopyright("Copyright © Raygun 2014-2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("ca9d9a01-c33b-4f17-af07-0305e86764c8")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
