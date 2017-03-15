43190ce701c329fd2bffcdef855141ab79ca0a7d
Taylor Lodge <ubermouse894@gmail.com>
2017-03-15 15:44:17 +1300

    Updated build and package scripts & created nuspec for Azure.WebJob package

--- build.ps1
+++ build.ps1
@@ -6,6 +6,7 @@ properties {
     $solution_file4 =                "$root/Mindscape.Raygun4Net4.sln"
     $solution_file4_client_profile = "$root/Mindscape.Raygun4Net4.ClientProfile.sln"
     $solution_file_mvc =             "$root/Mindscape.Raygun4Net.Mvc.sln"
+    $solution_file_webjob =          "$root/Mindscape.Raygun4Net.Azure.WebJob.sln"
     $solution_file_webapi =          "$root/Mindscape.Raygun4Net.WebApi.sln"
     $solution_file_winrt =           "$root/Mindscape.Raygun4Net.WinRT.sln"
     $solution_file_windows_phone =   "$root/Mindscape.Raygun4Net.WindowsPhone.sln"
@@ -16,6 +17,7 @@ properties {
     $build_dir4 =                    "$build_dir\Net4"
     $build_dir4_client_profile =     "$build_dir\Net4.ClientProfile"
     $build_dir_mvc =                 "$build_dir\Mvc"
+    $build_dir_webjob =              "$build_dir\WebJob"
     $build_dir_webapi =              "$build_dir\WebApi"
     $nunit_dir =                     "$root\packages\NUnit.Runners.2.6.2\tools\"
     $tools_dir =                     "$root\tools"
@@ -38,6 +40,7 @@ task Compile -depends Init {
     exec { msbuild "$solution_file2" /m /p:OutDir=$build_dir2 /p:Configuration=$configuration }
     exec { msbuild "$solution_file4" /m /p:OutDir=$build_dir4 /p:Configuration=$configuration }
     exec { msbuild "$solution_file_mvc" /m /p:OutDir=$build_dir_mvc /p:Configuration=$configuration }
+    exec { msbuild "$solution_file_webjob" /m /p:OutDir=$build_dir_webjob /p:Configuration=$configuration }
     exec { msbuild "$solution_file_webapi" /m /p:OutDir=$build_dir_webapi /p:Configuration=$configuration }
     
     exec { msbuild "$solution_file_client_profile" /m /p:OutDir=$build_dir_client_profile /p:Configuration=$configuration }
