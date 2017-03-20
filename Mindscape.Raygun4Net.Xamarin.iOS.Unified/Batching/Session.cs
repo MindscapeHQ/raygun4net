724295374c0ae75cc832b3659ed02c3a8650c098
QuantumNightmare <quantumnightmare@hotmail.com>
2017-03-21 10:34:05 +1300

    Offline model

---
+++ Mindscape.Raygun4Net.Xamarin.iOS.Unified/Batching/Session.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Mindscape.Raygun4Net.Messages;
+
+namespace Mindscape.Raygun4Net
+{
+  internal class Session
+  {
+    public DateTime Start { get; set; }
+
+    public DateTime? End { get; set; }
+
+    public string SessionId { get; set; }
+
+    public RaygunIdentifierMessage User { get; set; }
+
+    public string Version { get; set; }
+
+    public string OS { get; set; }
+
+    public string Platform { get; set; }
+
+    public List<SessionEvent> Events { get; set; }
+  }
+}
---
+++ Mindscape.Raygun4Net.Xamarin.iOS.Unified/Batching/SessionEvent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Mindscape.Raygun4Net
+{
+  internal class SessionEvent
+  {
+    public DateTime Timestamp { get; set; }
+
+    public string Name { get; set; }
+
+    public string Type { get; set; }
+
+    public decimal Duration { get; set; }
+  }
+}
