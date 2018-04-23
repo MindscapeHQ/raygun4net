440e42301c315b3334e78ca2d1a469dbb2f10d41
Mitchell Duncan <mitchell@raygun.com>
2018-04-23 14:35:52 +1200

    Adding assembly property files.

---
+++ Mindscape.Raygun4Net.NetCore/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Raygun4Net.NetCore")]
+[assembly: AssemblyDescription("Raygun provider for .NET Core projects")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Raygun")]
+[assembly: AssemblyProduct("Raygun4Net")]
+[assembly: AssemblyCopyright("Copyright © Raygun 2016-2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a5a4b4f9-058f-489e-9bd2-2d540247ead8")]
