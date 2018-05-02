10326a75862b34d18525b45b0e639d55265773d1
Mitchell Duncan <mitchell@raygun.com>
2018-05-03 09:23:15 +1200

    Adding breadcrumb tests.

---
+++ Mindscape.Raygun4Net.NetCore.Tests/RaygunBreadcrumbTestRecorder.cs
@@ -0,0 +1,23 @@
+﻿using Mindscape.Raygun4Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tests.Model // Namespace dowse not contain Mindscape.Raygun in order to be ignored by the breadcrumb stacktrace scanning logic.
+{
+  internal class RaygunBreadcrumbTestRecorder
+  {
+    private RaygunBreadcrumbs _breadcrumbs;
+
+    public RaygunBreadcrumbTestRecorder(RaygunBreadcrumbs breadcrumbs)
+    {
+      _breadcrumbs = breadcrumbs;
+    }
+
+    public void Record()
+    {
+      _breadcrumbs.Store(new Mindscape.Raygun4Net.RaygunBreadcrumb() { Message = "test" });
+    }
+  }
+}
---
+++ Mindscape.Raygun4Net.NetCore.Tests/RaygunBreadcrumbsTests.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Tests.Model;
+
+namespace Mindscape.Raygun4Net.Tests
+{
+  [TestFixture]
+  public class RaygunBreadcrumbsTests
+  {
+    [SetUp]
+    public void SetUp()
+    {
+    }
+
+    private RaygunBreadcrumbs GetBreadcrumbs(bool locationRecording = false, RaygunBreadcrumbLevel level = RaygunBreadcrumbLevel.Info)
+    {
+      var settings = new RaygunSettings() { BreadcrumbsLocationRecordingEnabled = locationRecording, BreadcrumbsLevel = level };
+      return new RaygunBreadcrumbs(settings, new RaygunInMemoryBreadcrumbStorage());
+    }
+
+    [Test]
+    public void Set_ClassName_MethodName_And_LineNumber_Automatically_If_Configured()
+    {
+      var breadcrumbs = GetBreadcrumbs(true);
+      var test = new RaygunBreadcrumbTestRecorder(breadcrumbs);
+
+      test.Record();
+      var crumb = breadcrumbs.First();
+
+      Assert.That(crumb.ClassName, Is.EqualTo("Tests.Model.RaygunBreadcrumbTestRecorder"));
+      Assert.That(crumb.MethodName, Is.EqualTo("Record"));
+    }
+
+    [Test]
+    public void You_Can_Record_A_Breadcrumb()
+    {
+      var breadcrumbs = GetBreadcrumbs();
+      breadcrumbs.Store(new RaygunBreadcrumb() { Message = "test" });
+
+      Assert.That(breadcrumbs.First().Message, Is.EqualTo("test"));
+    }
+
+    [Test]
+    public void It_Sets_The_Timestamp()
+    {
+      var breadcrumbs = GetBreadcrumbs();
+      breadcrumbs.Store(new RaygunBreadcrumb() { Message = "test" });
+
+      Assert.That(breadcrumbs.First().Timestamp, Is.GreaterThan(0));
+    }
+
+    [Test]
+    public void It_Sets_The_Level_To_Info_If_Not_Set()
+    {
+      var breadcrumbs = GetBreadcrumbs();
+      breadcrumbs.Store(new RaygunBreadcrumb() { Message = "test" });
+
+      Assert.That(breadcrumbs.First().Level, Is.EqualTo(RaygunBreadcrumbLevel.Info));
+    }
+
+    [Test]
+    public void It_Does_Not_Record_A_Breadcrumb_When_The_Breadcrumb_Level_Is_Too_High()
+    {
+      var breadcrumbs = GetBreadcrumbs(false, RaygunBreadcrumbLevel.Error);
+     
+      breadcrumbs.Store(new RaygunBreadcrumb() { Message = "test", Level = RaygunBreadcrumbLevel.Info });
+
+      Assert.That(breadcrumbs, Is.Empty);
+    }
+
+    [Test]
+    public void You_Can_Retrieve_Stored_Breadcrumbs()
+    {
+      var breadcrumbs = new RaygunBreadcrumbs(new RaygunSettings(), new RaygunInMemoryBreadcrumbStorage(new List<RaygunBreadcrumb>() { new RaygunBreadcrumb() }));
+
+      Assert.That(breadcrumbs.Count(), Is.EqualTo(1));
+    }
+
+    [Test]
+    public void You_Can_Clear_The_Stored_Breadcrumbs()
+    {
+      var breadcrumbs = new RaygunBreadcrumbs(new RaygunSettings(), new RaygunInMemoryBreadcrumbStorage(new List<RaygunBreadcrumb>() { new RaygunBreadcrumb() }));
+
+      breadcrumbs.Clear();
+
+      Assert.That(breadcrumbs.Count(), Is.EqualTo(0));
+    }
+  }
+}
