077371a95bd9c9d377a4805017d009c19a0e9b1a
Greg Pakes <greg.pakes@isams.co.uk>
2018-05-07 18:19:02 +1000

    Reintroduced the 4096 byte limit

--- Mindscape.Raygun4Net.WebApi/RaygunWebApiDelegatingHandler.cs
+++ Mindscape.Raygun4Net.WebApi/RaygunWebApiDelegatingHandler.cs
@@ -1,18 +1,22 @@
-﻿using System.Net.Http;
+﻿using System.Linq;
+using System.Net.Http;
+using System.Text;
 
 namespace Mindscape.Raygun4Net.WebApi
 {
-    public class RaygunWebApiDelegatingHandler : DelegatingHandler
+  public class RaygunWebApiDelegatingHandler : DelegatingHandler
   {
     public const string RequestBodyKey = "Raygun.RequestBody";
 
     protected override async System.Threading.Tasks.Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, System.Threading.CancellationToken cancellationToken)
     {
-      // ReadAsStringAsync is always readable as it calls LoadIntoBufferAsync internally.
-      var body = await request.Content.ReadAsStringAsync();
-      if (!string.IsNullOrEmpty(body))
+      // ReadAsByteArrayAsync is always readable as it calls LoadIntoBufferAsync internally.
+      var bytes = await request.Content.ReadAsByteArrayAsync();
+      if (bytes.Length > 0)
       {
-        request.Properties[RequestBodyKey] = body;
+        // Only take first 4096 bytes
+        var bytesToSend = bytes.Take(4096).ToArray();
+        request.Properties[RequestBodyKey] = Encoding.UTF8.GetString(bytesToSend);
       }
 
       return await base.SendAsync(request, cancellationToken);
