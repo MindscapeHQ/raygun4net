80dc85a30c4643b7a68c224e31ff03c3329f98d9
Jeremy Scott <jscott@raygun.com>
2018-05-23 09:56:36 +1200

    adds version information to AspNetCore provider

---
+++ Mindscape.Raygun4Net.AspNetCore/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Raygun4Net.AspNetCore")]
+[assembly: AssemblyDescription("Raygun provider for ASP.NET Core projects")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Raygun")]
+[assembly: AssemblyProduct("Raygun4Net")]
+[assembly: AssemblyCopyright("Copyright © Raygun 2016-2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cf9b3e00-389e-46ec-9922-b770aba249c0")]
