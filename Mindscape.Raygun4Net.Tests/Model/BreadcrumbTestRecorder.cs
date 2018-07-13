72ae6c4c6c525880f04fc32cfb0bdc3e304d8db8
QuantumNightmare <quantumnightmare@hotmail.com>
2018-07-13 15:15:27 +1200

    Readded readme.txt for NuGet packages

--- Mindscape.Raygun4Net.Tests/Model/BreadcrumbTestRecorder.cs
+++ Mindscape.Raygun4Net.Tests/Model/BreadcrumbTestRecorder.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Tests.Model // Namespace dowse not contain Mindscape.Raygun in order to be ignored by the breadcrumb stacktrace scanning logic.
+namespace Tests.Model // Namespace does not contain Mindscape.Raygun in order to be ignored by the breadcrumb stacktrace scanning logic.
 {
   internal class BreadcrumbTestRecorder
   {
