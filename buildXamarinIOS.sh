80303460f1ce38b1b9da5b738ddac8ad7c853ff2
Mitchell Duncan <mitchell@raygun.com>
2018-11-13 10:50:22 +1300

    Copy out the dll and nuget package.

--- buildXamarinIOS.sh
+++ buildXamarinIOS.sh
@@ -4,4 +4,5 @@ rm -rf Build/Xamarin/iOS
 "/Applications/Xamarin Studio.app/Contents/MacOS/mdtool" -v build "--target:Clean" "--configuration:Release|iPhone" "Mindscape.Raygun4Net.Xamarin.iOS.sln"
 "/Applications/Xamarin Studio.app/Contents/MacOS/mdtool" -v build "--configuration:Release|iPhone" "Mindscape.Raygun4Net.Xamarin.iOS.sln"
 mkdir -p Build/Xamarin/iOS
-cp -v Mindscape.Raygun4Net.Xamarin.iOS.Unified/bin/Release/Mindscape.Raygun4Net.Xamarin.iOS.Unified.dll Build/Xamarin/iOS/Mindscape.Raygun4Net.Xamarin.iOS.Unified.dll
+cp -v Mindscape.Raygun4Net.Xamarin.iOS.Unified/bin/Release/*.dll Build/Xamarin/iOS/
+cp -v Mindscape.Raygun4Net.Xamarin.iOS.Unified/bin/Release/*.nupkg Build/Xamarin/iOS/
