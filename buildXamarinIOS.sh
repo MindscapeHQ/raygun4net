9fd5bddb241c4acc94069adc9027ed256f138be9
Mitchell Duncan <mitchell@raygun.com>
2018-11-16 16:14:28 +1300

    Use visual studio in the build script. Xamarin Studio has been deprecated.

--- buildXamarinIOS.sh
+++ buildXamarinIOS.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 rm -rf Build/Xamarin/iOS
 # iOS
-"/Applications/Xamarin Studio.app/Contents/MacOS/mdtool" -v build "--target:Clean" "--configuration:Release|iPhone" "Mindscape.Raygun4Net.Xamarin.iOS.sln"
-"/Applications/Xamarin Studio.app/Contents/MacOS/mdtool" -v build "--configuration:Release|iPhone" "Mindscape.Raygun4Net.Xamarin.iOS.sln"
+"/Applications/Visual Studio.app/Contents/MacOS/vstool" -v build "--target:Clean" "--configuration:Release|iPhone" "Mindscape.Raygun4Net.Xamarin.iOS.sln"
+"/Applications/Visual Studio.app/Contents/MacOS/vstool" -v build "--configuration:Release|iPhone" "Mindscape.Raygun4Net.Xamarin.iOS.sln"
 mkdir -p Build/Xamarin/iOS
 cp -v Mindscape.Raygun4Net.Xamarin.iOS.Unified/bin/Release/*.dll Build/Xamarin/iOS/
 cp -v Mindscape.Raygun4Net.Xamarin.iOS.Unified/bin/Release/*.nupkg Build/Xamarin/iOS/
