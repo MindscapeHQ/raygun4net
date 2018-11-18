47dac43b1ae59c84e10ecc49b745b1e6d49bc03c
Mitchell Duncan <mitchell@raygun.com>
2018-11-19 11:20:24 +1300

    Correcting version.

--- Mindscape.Raygun4Net.Xamarin.iOS.Unified/Properties/AssemblyVersionInfo.cs
+++ Mindscape.Raygun4Net.Xamarin.iOS.Unified/Properties/AssemblyVersionInfo.cs
@@ -10,5 +10,5 @@ using System.Reflection;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.6.1")]
-[assembly: AssemblyFileVersion("5.6.1")]
+[assembly: AssemblyVersion("5.5.6")]
+[assembly: AssemblyFileVersion("5.5.6")]
