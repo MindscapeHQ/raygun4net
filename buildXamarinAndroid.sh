fa15653d6aca030b96129bd67b4f390fa38cd0e2
Mitchell Duncan <mitchell@raygun.com>
2018-11-19 15:37:40 +1300

    Add script to build Xamarin Android provider on mac.

---
+++ buildXamarinAndroid.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+rm -rf Build/Xamarin/iOS
+# iOS
+"/Applications/Visual Studio.app/Contents/MacOS/vstool" -v build "--target:Clean" "--configuration:Release" "Mindscape.Raygun4Net.Xamarin.Android.sln"
+"/Applications/Visual Studio.app/Contents/MacOS/vstool" -v build "--configuration:Release" "Mindscape.Raygun4Net.Xamarin.Android.sln"
+mkdir -p Build/Xamarin/Android
+cp -v Mindscape.Raygun4Net.Xamarin.Android/bin/Release/*.dll Build/Xamarin/Android/
+cp -v Mindscape.Raygun4Net.Xamarin.Android/bin/Release/*.nupkg Build/Xamarin/Android/
