143fa9f7446a6257641a47e6e829b0163c1ae4cd
Mitchell Duncan <mitchell@raygun.com>
2018-11-21 10:55:24 +1300

    Fix copy paste mistake. This should be for Android.

--- buildXamarinAndroid.sh
+++ buildXamarinAndroid.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-rm -rf Build/Xamarin/iOS
-# iOS
+rm -rf Build/Xamarin/Android
+# Android
 "/Applications/Visual Studio.app/Contents/MacOS/vstool" -v build "--target:Clean" "--configuration:Release" "Mindscape.Raygun4Net.Xamarin.Android.sln"
 "/Applications/Visual Studio.app/Contents/MacOS/vstool" -v build "--configuration:Release" "Mindscape.Raygun4Net.Xamarin.Android.sln"
 mkdir -p Build/Xamarin/Android
