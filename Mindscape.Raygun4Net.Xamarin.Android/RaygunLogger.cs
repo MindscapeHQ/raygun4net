b9a86fd393eb41198fede038382d92d3b3aa55e9
Mitchell Duncan <mitchell@raygun.com>
2018-12-06 16:56:10 +1300

    Adding logger class.

---
+++ Mindscape.Raygun4Net.Xamarin.Android/RaygunLogger.cs
@@ -0,0 +1,34 @@
+﻿using Android.Util;
+
+namespace Mindscape.Raygun4Net
+{
+  public class RaygunLogger
+  {
+    private static string TAG = "Raygun";
+
+    public static void Debug(string message)
+    {
+      Log.Debug(TAG, message);
+    }
+
+    public static void Info(string message)
+    {
+      Log.Info(TAG, message);
+    }
+
+    public static void Warning(string message)
+    {
+      Log.Warn(TAG, message);
+    }
+
+    public static void Error(string message)
+    {
+      Log.Error(TAG, message);
+    }
+
+    public static void Verbose(string message)
+    {
+      Log.Verbose(TAG, message);
+    }
+  }
+}
