c060ae44c6c7f37d351dc47e8718b0fa7a1901af
Mitchell Duncan <mitchell@raygun.com>
2018-12-11 14:53:08 +1300

    Update file manager to read/write reports to disk.

--- Mindscape.Raygun4Net.Xamarin.Android/RaygunFileManager.cs
+++ Mindscape.Raygun4Net.Xamarin.Android/RaygunFileManager.cs
@@ -1,7 +1,8 @@
-﻿using Java.IO;
-using System.Text;
+﻿using System.Text;
+using System.Collections.Generic;
 using Mindscape.Raygun4Net.Messages;
 using System;
+using System.IO;
 using Android.Content;
 using Android.Runtime;
 
@@ -9,114 +10,71 @@ namespace Mindscape.Raygun4Net
 {
   public class RaygunFileManager
   {
-    private const int MaxStoredReports = 64;
+    private const string RAYGUN_DIRECTORY = "RaygunIO";
+    private const int MAX_STORED_REPORTS = 64;
+    private int currentFileCounter = 0;
 
-    public RaygunFileManager(){}
-
-    public void SaveMessage(RaygunMessage message, Context context)
+    public void SaveCrashReport(RaygunMessage crashReport)
     {
       try
       {
-        var messageJson = SimpleJson.SerializeObject(message);
-
-        if (context != null)
+        // Can only save the report if we havnt reached the report count limit.
+        if (IsFileLimitReached(MAX_STORED_REPORTS))
         {
-          using (File dir = context.GetDir("RaygunIO", FileCreationMode.Private))
-          {
-            int number = 1;
-            string[] files = dir.List();
-            while (true)
-            {
-              bool exists = FileExists(files, "RaygunErrorMessage" + number + ".txt");
-              if (!exists)
-              {
-                string nextFileName = "RaygunErrorMessage" + (number + 1) + ".txt";
-                exists = FileExists(files, nextFileName);
-                if (exists)
-                {
-                  DeleteFile(dir, nextFileName);
-                }
-                break;
-              }
-              number++;
-            }
-            if (number == 11)
-            {
-              string firstFileName = "RaygunErrorMessage1.txt";
-              if (FileExists(files, firstFileName))
-              {
-                DeleteFile(dir, firstFileName);
-              }
-            }
-
-            using (File file = new File(dir, "RaygunErrorMessage" + number + ".txt"))
-            {
-              using (FileOutputStream stream = new FileOutputStream(file))
-              {
-                stream.Write(Encoding.ASCII.GetBytes(messageJson));
-                stream.Flush();
-                stream.Close();
-              }
-            }
-
-            RaygunLogger.Debug("Saved message: " + "RaygunErrorMessage" + number + ".txt");
-            RaygunLogger.Debug("File Count: " + dir.List().Length);
-          }
+          RaygunLogger.Warning("Failed to store crash report - Reached max crash reports stored on device");
+          return;
         }
+
+        // Convert to JSON text.
+        var reportJson = SimpleJson.SerializeObject(crashReport);
+
+        // Generate a unique name for our report.
+        var fileName = GetUniqueAcendingJsonName();
+
+        // Save the report to disk.
+        var filePath = StoreCrashReport(fileName, reportJson);
+
+        RaygunLogger.Debug("Saved message: " + filePath);
       }
       catch (Exception ex)
       {
-        RaygunLogger.Error(string.Format("Error saving message to isolated storage {0}", ex.Message));
+        RaygunLogger.Error(string.Format("Failed to store crash report - Error saving message to isolated storage {0}", ex.Message));
       }
     }
 
-    public void SendStoredMessages(Context context)
+    private string StoreCrashReport(string fileName, string data)
     {
+      // Generate the full file path.
+      var filePath = Path.Combine(GetCrashReportDirectory(), fileName);
+
+      // Write data to file path.
+      using (var writer = File.Create(filePath))
+      {
+        var bytes = Encoding.ASCII.GetBytes(data);
+        writer.Write(bytes, 0, bytes.Length);
+      }
+
+      return filePath;
+    }
+
+    public List<RaygunFile> GetAllStoredCrashReports()
+    {
+      var allReports = new List<RaygunFile>();
+
       try
       {
-        using (File dir = context.GetDir("RaygunIO", FileCreationMode.Private))
-        {
-          File[] files = dir.ListFiles();
-          foreach (File file in files)
-          {
-            if (file.Name.StartsWith("RaygunErrorMessage"))
-            {
-              using (FileInputStream stream = new FileInputStream(file))
-              {
-                using (InputStreamInvoker isi = new InputStreamInvoker(stream))
-                {
-                  using (InputStreamReader streamReader = new Java.IO.InputStreamReader(isi))
-                  {
-                    using (BufferedReader bufferedReader = new BufferedReader(streamReader))
-                    {
-                      StringBuilder stringBuilder = new StringBuilder();
-                      string line;
-                      while ((line = bufferedReader.ReadLine()) != null)
-                      {
-                        stringBuilder.Append(line);
-                      }
-                      bool success = RaygunClient.Current.SendMessage(stringBuilder.ToString());
-                      // If just one message fails to send, then don't delete the message, and don't attempt sending anymore until later.
-                      if (!success)
-                      {
-                        return;
-                      }
+        // Get the paths to all the files in the crash report directory.
+        var files = Directory.GetFiles(GetCrashReportDirectory());
 
-                      RaygunLogger.Debug("Sent " + file.Name);
-                    }
-                  }
-                }
-              }
-              file.Delete();
-            }
-          }
-          if (dir.List().Length == 0)
+        foreach (var filePath in files)
+        {
+          // Read the content of the file on disk.
+          var raygunFile = ReadCrashReportFromDisk(filePath);
+         
+          // Add it to our list if valid.
+          if (raygunFile != null && raygunFile.Data != null)
           {
-            if (files.Length > 0)
-            {
-              RaygunLogger.Debug("Successfully sent all pending messages");
-            }
-            dir.Delete();
+            allReports.Add(raygunFile);
           }
         }
       }
@@ -124,31 +82,77 @@ namespace Mindscape.Raygun4Net
       {
         RaygunLogger.Error(string.Format("Error sending stored messages to Raygun.io {0}", ex.Message));
       }
+
+      return allReports;
     }
 
-    private bool FileExists(string[] files, string fileName)
+    private RaygunFile ReadCrashReportFromDisk(string fileName)
     {
-      foreach (string str in files)
+      // Generate the full file path.
+      var filePath = Path.Combine(GetCrashReportDirectory(), fileName);
+
+      // Check a file exists with this path.
+      if (filePath == null || !File.Exists(filePath))
       {
-        if (fileName.Equals(str))
-        {
-          return true;
-        }
+        return null;
       }
-      return false;
+
+      var raygunFile = new RaygunFile(filePath);
+
+      // Read in the contents of the file.
+      using (var streamReader = new StreamReader(filePath))
+      {
+        raygunFile.Data = streamReader.ReadToEnd();
+      }
+
+      return raygunFile;
     }
 
-    private void DeleteFile(File dir, string fileName)
+    public void RemoveFile(string filePath)
     {
-      File[] files = dir.ListFiles();
-      foreach (File file in files)
+      // Validate the file path.
+      if (string.IsNullOrEmpty(filePath))
       {
-        if (fileName.Equals(file.Name))
-        {
-          file.Delete();
-          return;
-        }
+        RaygunLogger.Debug("Failed to remove file - Invalid file path");
+        return;
       }
+
+      // Check a file exists at this path.
+      if (!File.Exists(filePath))
+      {
+        RaygunLogger.Debug("Failed to remove file - File does not exist");
+        return;
+      }
+
+      try
+      {
+        File.Delete(filePath);
+      }
+      catch(Exception e)
+      {
+        RaygunLogger.Error("Failed to remove file - Due to error: " + e.Message); 
+      }
+    }
+
+    private bool IsFileLimitReached(int maxCount)
+    {
+      return NumberOfCrashReportsOnDisk() >= maxCount;
+    }
+
+    private int NumberOfCrashReportsOnDisk()
+    {
+      var files = Directory.GetFiles(GetCrashReportDirectory());
+      return files.Length;
+    }
+
+    private string GetCrashReportDirectory()
+    {
+      return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), RAYGUN_DIRECTORY);
+    }
+
+    private string GetUniqueAcendingJsonName()
+    {
+      return string.Format("{0}-{1}-{2}.json", DateTime.UtcNow.ToLongTimeString(), currentFileCounter++, Guid.NewGuid().ToString());
     }
   }
 }
