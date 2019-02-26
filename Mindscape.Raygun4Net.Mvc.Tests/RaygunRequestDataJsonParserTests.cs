ac8f1ca750cb5b5d3831866ae09f545b9c46006f
Mitchell Duncan <mitchell@raygun.com>
2019-02-26 16:08:00 +1300

    Adding some json parser tests.

---
+++ Mindscape.Raygun4Net.Mvc.Tests/RaygunRequestDataJsonParserTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using Mindscape.Raygun4Net.Parsers;
+
+namespace Mindscape.Raygun4Net.Mvc.Tests
+{
+  [TestFixture]
+  public class RaygunRequestDataJsonParserTests
+  {
+    RaygunRequestDataJsonParser _parser;
+
+    [SetUp]
+    public void SetUp()
+    {
+      _parser = new RaygunRequestDataJsonParser();
+    }
+
+    [Test]
+    public void ParseJsonContainingStringValues()
+    {
+      var data = "{\"MyKey\":\"MyValue\"}";
+      var dict = _parser.ToDictionary(data);
+
+      Assert.IsNotNull(dict);
+      Assert.AreEqual(dict.Count, 1);
+    }
+
+    [Test]
+    public void ParseJsonContainingNonStringValues()
+    {
+      var data = "{\"Url\":\"http://thing.com:1234\",\"ProjectId\":null,\"Enabled\":false}";
+      var dict = _parser.ToDictionary(data);
+
+      Assert.IsNotNull(dict);
+      Assert.AreEqual(dict.Count, 3);
+      Assert.AreEqual(dict["Url"], "http://thing.com:1234");
+      Assert.AreEqual(dict["ProjectId"], null);
+      Assert.AreEqual(dict["Enabled"], false);
+    }
+
+  }
+}
+
