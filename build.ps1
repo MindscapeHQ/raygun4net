2fdaa13dcf94d0da8a572f7d698ebad0e0c10699
Mitchell Duncan <mitchell@raygun.com>
2019-04-01 16:21:54 +1300

    Ensure libraries are built and placed into their respective platform folders.

--- build.ps1
+++ build.ps1
@@ -1,27 +1,28 @@
 properties {
-    $root =                          $psake.build_script_dir
-    $solution_file =                 "$root/Mindscape.Raygun4Net.sln"
-	$solution_file2 =                "$root/Mindscape.Raygun4Net2.sln"
-    $solution_file4 =                "$root/Mindscape.Raygun4Net4.sln"
-	$solution_file4_client_profile = "$root/Mindscape.Raygun4Net4.ClientProfile.sln"
-    $solution_file_client_profile =  "$root/Mindscape.Raygun4Net.ClientProfile.sln"
-    $solution_file_mvc =             "$root/Mindscape.Raygun4Net.Mvc.sln"
-	$solution_file_webapi =          "$root/Mindscape.Raygun4Net.WebApi.sln"
-    $solution_file_webjob =          "$root/Mindscape.Raygun4Net.Azure.WebJob.sln"
-    $solution_file_winrt =           "$root/Mindscape.Raygun4Net.WinRT.sln"
-    $configuration =                 "Release"
-    $build_dir =                     "$root\build\"
-    $build_dir2 =                    "$build_dir\Net2"
-    $build_dir4 =                    "$build_dir\Net4"
-    $build_dir4_client_profile =     "$build_dir\Net4.ClientProfile"
-	$build_dir_client_profile =      "$build_dir\Net3.ClientProfile"
-    $build_dir_mvc =                 "$build_dir\Mvc"
-    $build_dir_webapi =              "$build_dir\WebApi"
-    $build_dir_webjob =              "$build_dir\WebJob"
-	$nunit_dir =                     "$root\packages\NUnit.Runners.2.6.2\tools\"
-    $tools_dir =                     "$root\tools"
-    $nuget_dir =                     "$root\.nuget"
-    $env:Path +=                     ";$nunit_dir;$tools_dir;$nuget_dir"
+    $root =                              $psake.build_script_dir
+    $solution_file =                     "$root/Mindscape.Raygun4Net.sln"
+	$solution_file_net2 =                "$root/Mindscape.Raygun4Net2.sln"
+    $solution_file_net4 =                "$root/Mindscape.Raygun4Net4.sln"
+	$solution_file_net4_client_profile = "$root/Mindscape.Raygun4Net4.ClientProfile.sln"
+    $solution_file_net3_client_profile = "$root/Mindscape.Raygun4Net.ClientProfile.sln"
+    $solution_file_mvc =                 "$root/Mindscape.Raygun4Net.Mvc.sln"
+	$solution_file_webapi =              "$root/Mindscape.Raygun4Net.WebApi.sln"
+    $solution_file_webjob =              "$root/Mindscape.Raygun4Net.Azure.WebJob.sln"
+    $solution_file_winrt =               "$root/Mindscape.Raygun4Net.WinRT.sln"
+    $configuration =                     "Release"
+    $build_dir =                         "$root\build\"
+    $build_dir_net2 =                    "$build_dir\Net2"
+    $build_dir_net4 =                    "$build_dir\Net4"
+    $build_dir_net4_client_profile =     "$build_dir\Net4.ClientProfile"
+	$build_dir_net3_client_profile =     "$build_dir\Net3.ClientProfile"
+    $build_dir_mvc =                     "$build_dir\Mvc"
+    $build_dir_webapi =                  "$build_dir\WebApi"
+    $build_dir_webjob =                  "$build_dir\WebJob"
+	$build_dir_winrt =                   "$build_dir\WinRT"
+	$nunit_dir =                         "$root\packages\NUnit.Runners.2.6.2\tools\"
+    $tools_dir =                         "$root\tools"
+    $nuget_dir =                         "$root\.nuget"
+    $env:Path +=                         ";$nunit_dir;$tools_dir;$nuget_dir"
 }
 
 task default -depends Compile, CompileWinRT
@@ -36,24 +37,23 @@ task Init -depends Clean {
 
 task Compile -depends Init {
     exec { msbuild "$solution_file" /m /p:OutDir=$build_dir /p:Configuration=$configuration }
-    exec { msbuild "$solution_file2" /m /p:OutDir=$build_dir2 /p:Configuration=$configuration }
-    exec { msbuild "$solution_file4" /m /p:OutDir=$build_dir4 /p:Configuration=$configuration }
+    exec { msbuild "$solution_file_net2" /m /p:OutDir=$build_dir_net2 /p:Configuration=$configuration }
+    exec { msbuild "$solution_file_net4" /m /p:OutDir=$build_dir_net4 /p:Configuration=$configuration }
+	exec { msbuild "$solution_file_net3_client_profile" /m /p:OutDir=$build_dir_net3_client_profile /p:Configuration=$configuration }
+    exec { msbuild "$solution_file_net4_client_profile" /m /p:OutDir=$build_dir_net4_client_profile /p:Configuration=$configuration }
     exec { msbuild "$solution_file_mvc" /m /p:OutDir=$build_dir_mvc /p:Configuration=$configuration }
     exec { msbuild "$solution_file_webjob" /m /p:OutDir=$build_dir_webjob /p:Configuration=$configuration }
     exec { msbuild "$solution_file_webapi" /m /p:OutDir=$build_dir_webapi /p:Configuration=$configuration }
-    
-    exec { msbuild "$solution_file_client_profile" /m /p:OutDir=$build_dir_client_profile /p:Configuration=$configuration }
-    exec { msbuild "$solution_file4_client_profile" /m /p:OutDir=$build_dir4_client_profile /p:Configuration=$configuration }
 }
 
 task CompileWinRT -depends Init {
-    exec { msbuild "$solution_file_winrt" /m /p:OutDir=$build_dir /p:Configuration=$configuration }
-    move-item $build_dir/Mindscape.Raygun4Net.WinRT/Mindscape.Raygun4Net.WinRT.dll $build_dir
-    move-item $build_dir/Mindscape.Raygun4Net.WinRT/Mindscape.Raygun4Net.WinRT.pdb $build_dir
-    move-item $build_dir/Mindscape.Raygun4Net.WinRT.Tests/Mindscape.Raygun4Net.WinRT.Tests.dll $build_dir
-    move-item $build_dir/Mindscape.Raygun4Net.WinRT.Tests/Mindscape.Raygun4Net.WinRT.Tests.pdb $build_dir
-    remove-item -force -recurse $build_dir/Mindscape.Raygun4Net.WinRT -ErrorAction SilentlyContinue | Out-Null
-    remove-item -force -recurse $build_dir/Mindscape.Raygun4Net.WinRT.Tests -ErrorAction SilentlyContinue | Out-Null
+    exec { msbuild "$solution_file_winrt" /m /p:OutDir=$build_dir_winrt /p:Configuration=$configuration }
+    move-item $build_dir_winrt/Mindscape.Raygun4Net.WinRT/Mindscape.Raygun4Net.WinRT.dll $build_dir_winrt
+    move-item $build_dir_winrt/Mindscape.Raygun4Net.WinRT/Mindscape.Raygun4Net.WinRT.pdb $build_dir_winrt
+    move-item $build_dir_winrt/Mindscape.Raygun4Net.WinRT.Tests/Mindscape.Raygun4Net.WinRT.Tests.dll $build_dir_winrt
+    move-item $build_dir_winrt/Mindscape.Raygun4Net.WinRT.Tests/Mindscape.Raygun4Net.WinRT.Tests.pdb $build_dir_winrt
+    remove-item -force -recurse $build_dir_winrt/Mindscape.Raygun4Net.WinRT -ErrorAction SilentlyContinue | Out-Null
+    remove-item -force -recurse $build_dir_winrt/Mindscape.Raygun4Net.WinRT.Tests -ErrorAction SilentlyContinue | Out-Null
 }
 
 task Test -depends Compile, CompileWinRT {
--- buildSigned.ps1
+++ buildSigned.ps1
@@ -1,24 +1,25 @@
 properties {
-    $root =                             $psake.build_script_dir
-    $solution_file =                    "$root/Mindscape.Raygun4Net.sln"
-    $solution_file2 =                   "$root/Mindscape.Raygun4Net2.sln"
-    $solution_file4 =                   "$root/Mindscape.Raygun4Net4.sln"
-    $solution_file4_client_profile =    "$root/Mindscape.Raygun4Net4.ClientProfile.sln"
-	$solution_file_client_profile =     "$root/Mindscape.Raygun4Net.ClientProfile.sln"
-    $solution_file_mvc =                "$root/Mindscape.Raygun4Net.Mvc.sln"
-	$solution_file_webapi =             "$root/Mindscape.Raygun4Net.WebApi.sln"
-	$solution_file_winrt =              "$root/Mindscape.Raygun4Net.WinRT.sln" 
-    $configuration =                    "Sign"
-    $build_dir =                        "$root\build\"
-    $signed_build_dir =                 "$build_dir\signed"
-    $signed_build_dir2 =                "$build_dir\signed\Net2"
-    $signed_build_dir4 =                "$build_dir\signed\Net4"
-    $signed_build_dir4_client_profile = "$build_dir\signed\Net4.ClientProfile"
-	$signed_build_dir_client_profile =  "$build_dir\signed\Net3.ClientProfile"
-    $signed_build_mvc =                 "$build_dir\signed\Mvc"
-    $signed_build_webapi =              "$build_dir\signed\WebApi"
-    $nuget_dir =                        "$root\.nuget"
-    $env:Path +=                        ";$nuget_dir"
+    $root =                                 $psake.build_script_dir
+    $solution_file =                        "$root/Mindscape.Raygun4Net.sln"
+    $solution_file_net2 =                   "$root/Mindscape.Raygun4Net2.sln"
+    $solution_file_net4 =                   "$root/Mindscape.Raygun4Net4.sln"
+    $solution_file_net4_client_profile =    "$root/Mindscape.Raygun4Net4.ClientProfile.sln"
+	$solution_file_net3_client_profile =    "$root/Mindscape.Raygun4Net.ClientProfile.sln"
+    $solution_file_mvc =                    "$root/Mindscape.Raygun4Net.Mvc.sln"
+	$solution_file_webapi =                 "$root/Mindscape.Raygun4Net.WebApi.sln"
+	$solution_file_winrt =                  "$root/Mindscape.Raygun4Net.WinRT.sln" 
+    $configuration =                        "Sign"
+    $build_dir =                            "$root\build\"
+    $signed_build_dir =                     "$build_dir\signed"
+    $signed_build_dir_net2 =                "$build_dir\signed\Net2"
+    $signed_build_dir_net4 =                "$build_dir\signed\Net4"
+    $signed_build_dir_net4_client_profile = "$build_dir\signed\Net4.ClientProfile"
+	$signed_build_dir_net3_client_profile = "$build_dir\signed\Net3.ClientProfile"
+    $signed_build_dir_mvc =                 "$build_dir\signed\Mvc"
+    $signed_build_dir_webapi =              "$build_dir\signed\WebApi"
+	$signed_build_dir_winrt =               "$build_dir\signed\WinRT"
+    $nuget_dir =                            "$root\.nuget"
+    $env:Path +=                            ";$nuget_dir"
 }
 
 task default -depends Compile, CompileWinRT
@@ -33,22 +34,20 @@ task Init -depends Clean {
 
 task Compile -depends Init {
     exec { msbuild "$solution_file" /m /p:OutDir=$signed_build_dir /p:Configuration=$configuration }
-    exec { msbuild "$solution_file2" /m /p:OutDir=$signed_build_dir2 /p:Configuration=$configuration }
-    exec { msbuild "$solution_file4" /m /p:OutDir=$signed_build_dir4 /p:Configuration=$configuration }
-    
-    exec { msbuild "$solution_file_client_profile" /m /p:OutDir=$signed_build_dir_client_profile /p:Configuration=$configuration }
-    exec { msbuild "$solution_file4_client_profile" /m /p:OutDir=$signed_build_dir4_client_profile /p:Configuration=$configuration }
-    
-    exec { msbuild "$solution_file_mvc" /m /p:OutDir=$signed_build_mvc /p:Configuration=$configuration }
-    exec { msbuild "$solution_file_webapi" /m /p:OutDir=$signed_build_webapi /p:Configuration=$configuration }
+    exec { msbuild "$solution_file_net2" /m /p:OutDir=$signed_build_dir_net2 /p:Configuration=$configuration }
+    exec { msbuild "$solution_file_net4" /m /p:OutDir=$signed_build_dir_net4 /p:Configuration=$configuration }
+    exec { msbuild "$solution_file_net3_client_profile" /m /p:OutDir=$signed_build_dir_net3_client_profile /p:Configuration=$configuration }
+    exec { msbuild "$solution_file)net4_client_profile" /m /p:OutDir=$signed_build_dir_net4_client_profile /p:Configuration=$configuration }
+    exec { msbuild "$solution_file_mvc" /m /p:OutDir=$signed_build_dir_mvc /p:Configuration=$configuration }
+    exec { msbuild "$solution_file_webapi" /m /p:OutDir=$signed_build_dir_webapi /p:Configuration=$configuration }
 }
 
 task CompileWinRT -depends Init {
-    exec { msbuild "$solution_file_winrt" /m /p:OutDir=$signed_build_dir /p:Configuration=$configuration }
-    move-item $signed_build_dir/Mindscape.Raygun4Net.WinRT/Mindscape.Raygun4Net.WinRT.dll $signed_build_dir
-    move-item $signed_build_dir/Mindscape.Raygun4Net.WinRT/Mindscape.Raygun4Net.WinRT.pdb $signed_build_dir
-    move-item $signed_build_dir/Mindscape.Raygun4Net.WinRT.Tests/Mindscape.Raygun4Net.WinRT.Tests.dll $signed_build_dir
-    move-item $signed_build_dir/Mindscape.Raygun4Net.WinRT.Tests/Mindscape.Raygun4Net.WinRT.Tests.pdb $signed_build_dir
-    remove-item -force -recurse $signed_build_dir/Mindscape.Raygun4Net.WinRT -ErrorAction SilentlyContinue | Out-Null
-    remove-item -force -recurse $signed_build_dir/Mindscape.Raygun4Net.WinRT.Tests -ErrorAction SilentlyContinue | Out-Null
+    exec { msbuild "$solution_file_winrt" /m /p:OutDir=$signed_build_dir_winrt /p:Configuration=$configuration }
+    move-item $signed_build_dir_winrt/Mindscape.Raygun4Net.WinRT/Mindscape.Raygun4Net.WinRT.dll $signed_build_dir_winrt
+    move-item $signed_build_dir_winrt/Mindscape.Raygun4Net.WinRT/Mindscape.Raygun4Net.WinRT.pdb $signed_build_dir_winrt
+    move-item $signed_build_dir_winrt/Mindscape.Raygun4Net.WinRT.Tests/Mindscape.Raygun4Net.WinRT.Tests.dll $signed_build_dir_winrt
+    move-item $signed_build_dir_winrt/Mindscape.Raygun4Net.WinRT.Tests/Mindscape.Raygun4Net.WinRT.Tests.pdb $signed_build_dir_winrt
+    remove-item -force -recurse $signed_build_dir_winrt/Mindscape.Raygun4Net.WinRT -ErrorAction SilentlyContinue | Out-Null
+    remove-item -force -recurse $signed_build_dir_winrt/Mindscape.Raygun4Net.WinRT.Tests -ErrorAction SilentlyContinue | Out-Null
 }
