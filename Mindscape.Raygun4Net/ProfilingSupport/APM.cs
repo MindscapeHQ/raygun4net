c288a269238569b99bba1de98ff740d3e12546b9
Matt Warren <matt.warren@live.co.uk>
2019-07-18 00:58:02 +0100

    Ensure we read the correct Environment variables for .NET Core

--- Mindscape.Raygun4Net/ProfilingSupport/APM.cs
+++ Mindscape.Raygun4Net/ProfilingSupport/APM.cs
@@ -27,7 +27,20 @@ namespace Mindscape.Raygun4Net
 
     public static bool ProfilerAttached
     {
-      get { return Environment.GetEnvironmentVariable("COR_PROFILER") == "{e2338988-38cc-48cd-a6b6-b441c31f34f1}" && Environment.GetEnvironmentVariable("COR_ENABLE_PROFILING") == "1"; }
+      get
+      {
+#if NETSTANDARD1_6 || NETSTANDARD2_0
+        // Look for .NET CORE compatible Environment Variables
+        return 
+          Environment.GetEnvironmentVariable("CORECLR_PROFILER") == "{e2338988-38cc-48cd-a6b6-b441c31f34f1}" &&
+          Environment.GetEnvironmentVariable("CORECLR_ENABLE_PROFILING") == "1";
+#else
+        // Look for .NET FRAMEWORK compatible Environment Variables
+        return
+          Environment.GetEnvironmentVariable("COR_PROFILER") == "{e2338988-38cc-48cd-a6b6-b441c31f34f1}" &&
+          Environment.GetEnvironmentVariable("COR_PROFILER") == "1";
+#endif
+      }
     }
   }
 }
