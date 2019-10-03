c54d67c277fc803fc7f270009156fd16a30a7226
Mitchell <mitchell@raygun.com>
2019-10-03 16:14:54 +1300

    Convert tabs to spaces
    Convert tabs to spaces

--- buildNetCore.ps1
+++ buildNetCore.ps1
@@ -5,8 +5,8 @@ properties {
     $build_dir_net_core =               "$build_dir\netcore"
     $build_dir_aspnet_core  =           "$build_dir\asp-netcore"
     $build_dir_net_core_common  =       "$build_dir\netcore-common"
-	$build_dir_signed_net_core =        "$build_dir\signed\netcore"
-	$build_dir_signed_net_core_common = "$build_dir\signed\netcore-common"
+    $build_dir_signed_net_core =        "$build_dir\signed\netcore"
+    $build_dir_signed_net_core_common = "$build_dir\signed\netcore-common"
     $nunit_dir =                        "$root\packages\NUnit.Runners.2.6.2\tools\"
     $tools_dir =                        "$root\tools"
     $nuget_dir =                        "$root\.nuget"
@@ -19,16 +19,16 @@ task Clean {
     remove-item -force -recurse $build_dir_net_core -ErrorAction SilentlyContinue | Out-Null
     remove-item -force -recurse $build_dir_aspnet_core -ErrorAction SilentlyContinue | Out-Null
     remove-item -force -recurse $build_dir_net_core_common -ErrorAction SilentlyContinue | Out-Null
-	remove-item -force -recurse $build_dir_signed_net_core -ErrorAction SilentlyContinue | Out-Null
-	remove-item -force -recurse $build_dir_signed_net_core_common -ErrorAction SilentlyContinue | Out-Null
+    remove-item -force -recurse $build_dir_signed_net_core -ErrorAction SilentlyContinue | Out-Null
+    remove-item -force -recurse $build_dir_signed_net_core_common -ErrorAction SilentlyContinue | Out-Null
 }
 
 task Init -depends Clean {
     new-item $build_dir_net_core -itemType directory | Out-Null
     new-item $build_dir_aspnet_core -itemType directory | Out-Null
     new-item $build_dir_net_core_common -itemType directory | Out-Null
-	new-item $build_dir_signed_net_core -itemType directory | Out-Null
-	new-item $build_dir_signed_net_core_common -itemType directory | Out-Null
+    new-item $build_dir_signed_net_core -itemType directory | Out-Null
+    new-item $build_dir_signed_net_core_common -itemType directory | Out-Null
 }
 
 task Compile -depends Init {
@@ -41,11 +41,11 @@ task Compile -depends Init {
     exec { dotnet pack .\Mindscape.Raygun4Net.AspNetCore\ --output build\AspNetCore --configuration Release }
     move-item -Path $root\Mindscape.Raygun4Net.AspNetCore\build\AspNetCore\* -Destination $build_dir_aspnet_core
 
-	# Signed
-	exec { dotnet pack .\Mindscape.Raygun4Net.NetCore.Common\ --output build\Signed\NetCoreCommon --configuration Sign }
+    # Signed
+    exec { dotnet pack .\Mindscape.Raygun4Net.NetCore.Common\ --output build\Signed\NetCoreCommon --configuration Sign }
     move-item -Path $root\Mindscape.Raygun4Net.NetCore.Common\build\Signed\NetCoreCommon\* -Destination $build_dir_signed_net_core_common
     
-	# Signed
+    # Signed
     exec { dotnet pack .\Mindscape.Raygun4Net.NetCore\ --output build\Signed\NetCore --configuration Sign }
     move-item -Path $root\Mindscape.Raygun4Net.NetCore\build\Signed\NetCore\* -Destination $build_dir_signed_net_core
 }
