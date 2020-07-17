55ba5b22c806ef5891550bad42c1bc98687b905a
Mitchell <mitchell@raygun.com>
2020-07-17 13:55:41 +1200

    Ensure API backwards compatibility
    Ensure API backwards compatibility

--- Mindscape.Raygun4Net2/RaygunClient.cs
+++ Mindscape.Raygun4Net2/RaygunClient.cs
@@ -248,7 +248,7 @@ namespace Mindscape.Raygun4Net
     /// <param name="raygunMessage">The RaygunMessage to send. This needs its OccurredOn property
     /// set to a valid DateTime and as much of the Details property as is available.</param>
     /// <param name="exception">The original exception that gernerated the RaygunMessage</param>
-    public void SendInBackground(RaygunMessage raygunMessage, Exception exception)
+    public void SendInBackground(RaygunMessage raygunMessage, Exception exception = null)
     {
       ThreadPool.QueueUserWorkItem(c => Send(raygunMessage, exception));
     }
