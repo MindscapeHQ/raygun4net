b408c91bf321a26a0687bf3601e71926e7f7a15c
Mitchell <mitchell@raygun.com>
2020-07-17 14:49:35 +1200

    Must declare properties to build WindowsStore
    Must declare properties to build WindowsStore

--- Mindscape.Raygun4Net/RaygunSendingMessageEventArgs.cs
+++ Mindscape.Raygun4Net/RaygunSendingMessageEventArgs.cs
@@ -9,13 +9,23 @@ namespace Mindscape.Raygun4Net
   /// </summary>
   public class RaygunSendingMessageEventArgs : CancelEventArgs
   {
+    private RaygunMessage _raygunMessage;
+    private Exception _exception;
+    
     public RaygunSendingMessageEventArgs(RaygunMessage message, Exception exception)
     {
-      Message = message;
-      Exception = exception;
+      _raygunMessage = message;
+      _exception = exception;
     }
 
-    public RaygunMessage Message { get; }
-    public Exception Exception { get; }
+    public RaygunMessage Message
+    {
+      get { return _raygunMessage; }
+    }
+    
+    public Exception Exception
+    {
+      get { return _exception; }
+    }
   }
 }
