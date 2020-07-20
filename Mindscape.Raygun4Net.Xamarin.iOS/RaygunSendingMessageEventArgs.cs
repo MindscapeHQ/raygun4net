e29789786cb66c4308804c6e8a113b75f1297584
Mitchell Duncan <mitchell@raygun.com>
2020-07-20 15:23:23 +1200

    Separating Xamarin iOS from changes.
    Separating Xamarin iOS from changes.

---
+++ Mindscape.Raygun4Net.Xamarin.iOS/RaygunSendingMessageEventArgs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+using Mindscape.Raygun4Net.Messages;
+
+namespace Mindscape.Raygun4Net
+{
+  /// <summary>
+  /// Can be used to modify the message before sending, or to cancel the send operation.
+  /// </summary>
+  public class RaygunSendingMessageEventArgs : CancelEventArgs
+  {
+    private RaygunMessage _raygunMessage;
+    
+    public RaygunSendingMessageEventArgs(RaygunMessage message)
+    {
+      _raygunMessage = message;
+    }
+
+    public RaygunMessage Message
+    {
+      get { return _raygunMessage; }
+    }
+  }
+}
