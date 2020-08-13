3c2767a34ae7ced3fbd0a5aed9859fef2bd1b30f
Mitchell <mitchell@raygun.com>
2020-08-13 15:24:50 +1200

    Updated assembly info and versions
    Updated assembly info and versions

--- Mindscape.Raygun4Net.NetCore/Properties/AssemblyVersionInfo.cs
+++ Mindscape.Raygun4Net.NetCore/Properties/AssemblyVersionInfo.cs
@@ -1,6 +1,4 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 // Version information for an assembly consists of the following four values:
 //
@@ -12,5 +10,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("6.3.0")]
-[assembly: AssemblyFileVersion("6.3.0")]
+[assembly: AssemblyVersion("6.3.1")]
+[assembly: AssemblyFileVersion("6.3.1")]
