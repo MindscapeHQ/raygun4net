8388608d0c4d3b952182462491e3937dfe05a83a
Darcy Thomas <dthomas@raygun.com>
2022-02-22 14:46:30 +1300

    Update buildNetCore.ps1

--- buildNetCore.ps1
+++ buildNetCore.ps1
@@ -32,33 +32,34 @@ task Init -depends Clean {
     new-item $build_dir_signed_net_core -itemType directory | Out-Null
     new-item $build_dir_signed_net_core_common -itemType directory | Out-Null
     new-item $build_dir_signed_aspnet_core -itemType directory | Out-Null
+
+    New-Item -ItemType Directory -Force -Path $root\Mindscape.Raygun4Net.NetCore.Common\build\NetCoreCommon\ | Out-Null
+    New-Item -ItemType Directory -Force -Path $root\Mindscape.Raygun4Net.NetCore\build\NetCore\ | Out-Null
+    New-Item -ItemType Directory -Force -Path $root\Mindscape.Raygun4Net.AspNetCore\build\AspNetCore\ | Out-Null
+    New-Item -ItemType Directory -Force -Path $root\Mindscape.Raygun4Net.NetCore.Common\build\Signed\NetCoreCommon\ | Out-Null
+    New-Item -ItemType Directory -Force -Path $root\Mindscape.Raygun4Net.NetCore\build\Signed\NetCore\ | Out-Null
+    New-Item -ItemType Directory -Force -Path $root\Mindscape.Raygun4Net.AspNetCore\build\Signed\AspNetCore\ | Out-Null
 }
 
 task Compile -depends Init {    
-    New-Item -ItemType Directory -Force -Path $root\Mindscape.Raygun4Net.NetCore.Common\build\NetCoreCommon\ | Out-Null
     exec { dotnet pack .\Mindscape.Raygun4Net.NetCore.Common\ --output build\NetCoreCommon --configuration Release }
     move-item -Path $root\Mindscape.Raygun4Net.NetCore.Common\build\NetCoreCommon\* -Destination $build_dir_net_core_common
     
-    New-Item -ItemType Directory -Force -Path $root\Mindscape.Raygun4Net.NetCore\build\NetCore\ | Out-Null
     exec { dotnet pack .\Mindscape.Raygun4Net.NetCore\ --output build\NetCore --configuration Release }
     move-item -Path $root\Mindscape.Raygun4Net.NetCore\build\NetCore\* -Destination $build_dir_net_core
     
-    New-Item -ItemType Directory -Force -Path $root\Mindscape.Raygun4Net.AspNetCore\build\AspNetCore\ | Out-Null
     exec { dotnet pack .\Mindscape.Raygun4Net.AspNetCore\ --output build\AspNetCore --configuration Release }
     move-item -Path $root\Mindscape.Raygun4Net.AspNetCore\build\AspNetCore\* -Destination $build_dir_aspnet_core
 
     # Signed
-    New-Item -ItemType Directory -Force -Path $root\Mindscape.Raygun4Net.NetCore.Common\build\Signed\NetCoreCommon\ | Out-Null
     exec { dotnet pack .\Mindscape.Raygun4Net.NetCore.Common\ --output build\Signed\NetCoreCommon --configuration Sign }
     move-item -Path $root\Mindscape.Raygun4Net.NetCore.Common\build\Signed\NetCoreCommon\* -Destination $build_dir_signed_net_core_common
     
     # Signed
-    New-Item -ItemType Directory -Force -Path $root\Mindscape.Raygun4Net.NetCore\build\Signed\NetCore\ | Out-Null
     exec { dotnet pack .\Mindscape.Raygun4Net.NetCore\ --output build\Signed\NetCore --configuration Sign }
     move-item -Path $root\Mindscape.Raygun4Net.NetCore\build\Signed\NetCore\* -Destination $build_dir_signed_net_core
 	
     # Signed
-    New-Item -ItemType Directory -Force -Path $root\Mindscape.Raygun4Net.AspNetCore\build\Signed\AspNetCore\ | Out-Null
     exec { dotnet pack .\Mindscape.Raygun4Net.AspNetCore\ --output build\Signed\AspNetCore --configuration Sign }
     move-item -Path $root\Mindscape.Raygun4Net.AspNetCore\build\Signed\AspNetCore\* -Destination $build_dir_signed_aspnet_core
 }
