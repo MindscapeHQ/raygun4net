48ce3989db333d77547373f6c472fbc45d3ad06d
Darcy Thomas <dthomas@raygun.com>
2022-02-22 15:07:35 +1300

    standard indentation

--- Mindscape.Raygun4Net.NetCore.Common/RaygunSettings.cs
+++ Mindscape.Raygun4Net.NetCore.Common/RaygunSettings.cs
@@ -2,29 +2,29 @@
 
 namespace Mindscape.Raygun4Net
 {
-    public abstract class RaygunSettingsBase
+  public abstract class RaygunSettingsBase
+  {
+    internal const string DefaultApiEndPoint = "https://api.raygun.com/entries";
+
+    public RaygunSettingsBase()
     {
-        internal const string DefaultApiEndPoint = "https://api.raygun.com/entries";
-
-        public RaygunSettingsBase()
-        {
-            ApiEndpoint = new Uri(DefaultApiEndPoint);
-        }
-
-        public string ApiKey { get; set; }
-
-        public Uri ApiEndpoint { get; set; }
-
-        public bool ThrowOnError { get; set; }
-
-        public string ApplicationVersion { get; set; }
-
-        /// <summary>
-        /// If set to true will automatically setup handlers to catch Unhandled Exceptions  
-        /// </summary>
-        /// <remarks>
-        /// Currently defaults to false. This may be change in future releases.
-        /// </remarks>
-        public bool CatchUnhandledExceptions { get; set; } = false;
+      ApiEndpoint = new Uri(DefaultApiEndPoint);
     }
+
+    public string ApiKey { get; set; }
+
+    public Uri ApiEndpoint { get; set; }
+
+    public bool ThrowOnError { get; set; }
+
+    public string ApplicationVersion { get; set; }
+
+    /// <summary>
+    /// If set to true will automatically setup handlers to catch Unhandled Exceptions  
+    /// </summary>
+    /// <remarks>
+    /// Currently defaults to false. This may be change in future releases.
+    /// </remarks>
+    public bool CatchUnhandledExceptions { get; set; } = false;
+  }
 }
