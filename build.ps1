5436a3f7b411d9eab0a8594de38961ea4fc9f5d8
Darcy Thomas <dthomas@raygun.com>
2023-05-18 07:35:59 +1200

    Bump version numbers
    Nuget tests
    
    less ws
    
    Less UWP for win 8
    
    it all builds! Apart from the win store
    
    wip
    
    bump major version
    
    fix
    
    Update Mindscape.Raygun4Net.Common.csproj
    
    Update Mindscape.Raygun4Net.Azure.WebJob.csproj
    
    less .net 3.0
    
    tidy
    
    Remove .Net 2-3.5 and windows phone
    
    Update Mindscape.Raygun4Net.AspNetCore.sln
    
    Update readme.txt
    
    Update README.md
    
    now can package
    
    Better building
    
    Now with moar tls!
    
    typo
    
    Update readme.txt
    
    Update README.md
    
    bump major version + getting psake builds to work correctly
    
    Tidys
    
    Update TestIfWeCanSendToRaygun.cs
    
    Removed the tests which proved what the behavior was before we did these changes
    
    Tidy
    
    Fix for broken tests
    
    Now even better!
    
    getting close
    
    Update WebClientHelper.cs
    
    Extracting interface
    
    Ok it builds
    
    porting moar tests
    
    Tidy
    
    wip
    
    clean up
    
    new integration tests
    
    Basic  integrations tests setup
    
    Now with green tests!
    
    Got tests properly done
    
    partial fix
    
    fix to tests to validate short comings of old code
    
    tidy
    
    wip tidy up
    
    Update UnitTest1.cs
    
    tidy
    
    Update TestIfWeCanSendToRaygun.cs
    
    Update UnitTest1.cs
    
    Update UnitTest1.cs
    
    Update RaygunClient.cs
    
    Post merge fix
    
    Tidy
    
    Bump version
    
    Update Mindscape.Raygun4Net4.Nuget.Tests.sln
    
    Fixed icon and license metadata to new spec
    
    Moar signing
    
    Update README.md
    
    Fix icons and license details
    
    Fix references
    
    moar signing
    
    Sign by default in release builds

--- build.ps1
+++ build.ps1
@@ -8,21 +8,22 @@ properties {
     $solution_file_mvc =          "$root/Mindscape.Raygun4Net.Mvc.sln"
     $solution_file_webapi =       "$root/Mindscape.Raygun4Net.WebApi.sln"
     $solution_file_webjob =       "$root/Mindscape.Raygun4Net.Azure.WebJob.sln"
-    $configuration =              "Sign" ## We can sign the regular packages too
-    $configurationOld =           "Release" ## Update some time in the future
+    $solution_file_winrt =        "$root/Mindscape.Raygun4Net.WinRT.sln"
+    $configuration =              "Release"
     $build_dir =                  "$root\build\"
     $build_dir_net4 =             "$build_dir\net40"
     $build_dir_net4_client =      "$build_dir\net40-client"
     $build_dir_mvc =              "$build_dir\mvc"
     $build_dir_webapi =           "$build_dir\webapi"
     $build_dir_webjob =           "$build_dir\webjob"
+   # $build_dir_winrt =            "$build_dir\winrt"
     $nunit_dir =                  "$root\packages\NUnit.Runners.2.6.2\tools\"
     $tools_dir =                  "$root\tools"
     $nuget_dir =                  "$root\.nuget"
     $env:Path +=                  ";$nunit_dir;$tools_dir;$nuget_dir"
 }
 
-task default -depends Compile 
+task default -depends Compile #, CompileWinRT
 
 task Clean {
     remove-item -force -recurse $build_dir -ErrorAction SilentlyContinue | Out-Null
@@ -36,15 +37,22 @@ task Compile -depends Init {
     
     exec { msbuild "$solution_file_net4"   /p:OutDir=$build_dir_net4 /p:Configuration=$configuration }
     exec { msbuild "$solution_file_net4_client" /m /p:OutDir=$build_dir_net4_client /p:Configuration=$configuration }
-
-    exec { msbuild "$solution_file_mvc" /m /p:OutDir=$build_dir_mvc /p:Configuration=$configurationOld }
-    exec { msbuild "$solution_file_webapi" /m /p:OutDir=$build_dir_webapi /p:Configuration=$configurationOld }
-    exec { msbuild "$solution_file_webjob" /m /p:OutDir=$build_dir_webjob /p:Configuration=$configurationOld }
+    exec { msbuild "$solution_file_mvc" /m /p:OutDir=$build_dir_mvc /p:Configuration=$configuration }
+    exec { msbuild "$solution_file_webapi" /m /p:OutDir=$build_dir_webapi /p:Configuration=$configuration }
+    exec { msbuild "$solution_file_webjob" /m /p:OutDir=$build_dir_webjob /p:Configuration=$configuration }
 }
 
+ task CompileWinRT -depends Init {
+     exec { msbuild "$solution_file_winrt" /m /p:OutDir=$build_dir_winrt /p:Configuration=$configuration }
+     move-item $build_dir_winrt/Mindscape.Raygun4Net.WinRT/Mindscape.Raygun4Net.WinRT.dll $build_dir_winrt
+     move-item $build_dir_winrt/Mindscape.Raygun4Net.WinRT/Mindscape.Raygun4Net.WinRT.pdb $build_dir_winrt
+     move-item $build_dir_winrt/Mindscape.Raygun4Net.WinRT.Tests/Mindscape.Raygun4Net.WinRT.Tests.dll $build_dir_winrt
+     move-item $build_dir_winrt/Mindscape.Raygun4Net.WinRT.Tests/Mindscape.Raygun4Net.WinRT.Tests.pdb $build_dir_winrt
+     remove-item -force -recurse $build_dir_winrt/Mindscape.Raygun4Net.WinRT -ErrorAction SilentlyContinue | Out-Null
+     remove-item -force -recurse $build_dir_winrt/Mindscape.Raygun4Net.WinRT.Tests -ErrorAction SilentlyContinue | Out-Null
+}
 
-
-task Test -depends Compile {
+task Test -depends Compile, CompileWinRT {
     $test_assemblies = Get-ChildItem $build_dir -Include *Tests.dll -Name
 
     Push-Location -Path $build_dir
