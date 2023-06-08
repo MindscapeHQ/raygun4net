34e05b29c2d6f7d5845c29db396757e6c43a2036
Darcy Thomas <dthomas@raygun.com>
2023-06-09 09:37:25 +1200

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -8,7 +8,6 @@ properties {
     $solution_file_mvc =          "$root/Mindscape.Raygun4Net.Mvc.sln"
     $solution_file_webapi =       "$root/Mindscape.Raygun4Net.WebApi.sln"
     $solution_file_webjob =       "$root/Mindscape.Raygun4Net.Azure.WebJob.sln"
-    $solution_file_winrt =        "$root/Mindscape.Raygun4Net.WinRT.sln"
     $configuration =              "Release"
     $build_dir =                  "$root\build\"
     $build_dir_net4 =             "$build_dir\net40"
@@ -16,14 +15,13 @@ properties {
     $build_dir_mvc =              "$build_dir\mvc"
     $build_dir_webapi =           "$build_dir\webapi"
     $build_dir_webjob =           "$build_dir\webjob"
-   # $build_dir_winrt =            "$build_dir\winrt"
     $nunit_dir =                  "$root\packages\NUnit.Runners.2.6.2\tools\"
     $tools_dir =                  "$root\tools"
     $nuget_dir =                  "$root\.nuget"
     $env:Path +=                  ";$nunit_dir;$tools_dir;$nuget_dir"
 }
 
-task default -depends Compile #, CompileWinRT
+task default -depends Compile
 
 task Clean {
     remove-item -force -recurse $build_dir -ErrorAction SilentlyContinue | Out-Null
@@ -42,17 +40,9 @@ task Compile -depends Init {
     exec { msbuild "$solution_file_webjob" /m /p:OutDir=$build_dir_webjob /p:Configuration=$configuration }
 }
 
- task CompileWinRT -depends Init {
-     exec { msbuild "$solution_file_winrt" /m /p:OutDir=$build_dir_winrt /p:Configuration=$configuration }
-     move-item $build_dir_winrt/Mindscape.Raygun4Net.WinRT/Mindscape.Raygun4Net.WinRT.dll $build_dir_winrt
-     move-item $build_dir_winrt/Mindscape.Raygun4Net.WinRT/Mindscape.Raygun4Net.WinRT.pdb $build_dir_winrt
-     move-item $build_dir_winrt/Mindscape.Raygun4Net.WinRT.Tests/Mindscape.Raygun4Net.WinRT.Tests.dll $build_dir_winrt
-     move-item $build_dir_winrt/Mindscape.Raygun4Net.WinRT.Tests/Mindscape.Raygun4Net.WinRT.Tests.pdb $build_dir_winrt
-     remove-item -force -recurse $build_dir_winrt/Mindscape.Raygun4Net.WinRT -ErrorAction SilentlyContinue | Out-Null
-     remove-item -force -recurse $build_dir_winrt/Mindscape.Raygun4Net.WinRT.Tests -ErrorAction SilentlyContinue | Out-Null
-}
 
-task Test -depends Compile, CompileWinRT {
+
+task Test -depends Compile {
     $test_assemblies = Get-ChildItem $build_dir -Include *Tests.dll -Name
 
     Push-Location -Path $build_dir
