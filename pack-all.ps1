cf59cf91367cb2708dcbe3e9b9bc0c523d222208
phillip-haydon <phillip.haydon@gmail.com>
2023-09-11 18:53:20 +0800

    Fixed versioning of FileVersion/ProductVersion for old non-sdk style projects

--- pack-all.ps1
+++ pack-all.ps1
@@ -8,6 +8,7 @@ properties {
     $nuspec_folder = "$root\nuspecs"
     $version = "7.0.0"
     $include_signed = $true
+    $global_assembly_info = ".\GlobalAssemblyInfo.cs" 
 }
 
 task default -depends Pack
@@ -17,16 +18,20 @@ task Clean {
 }
 
 task Build -depends Clean {
-    & dotnet build $root\Raygun.CrashReporting.sln -c Release
+    # Replace AssemblyVersion and AssemblyFileVersion
+    (Get-Content $global_assembly_info) -replace 'Version\("[^"]*"\)\]', "Version(`"$version`")]" | Set-Content $global_assembly_info
+
+    & dotnet clean $root\Raygun.CrashReporting.sln -c Release
+    & dotnet build $root\Raygun.CrashReporting.sln -c Release -p:FileVersion=$version -p:Version=$version
 }
 
 task Pack -depends Build {
 
     # Pack .NET SDK Projects (these use the project files and not nuspecs)
     # Because of old style projects we need to pack the specific projects directly
-    & dotnet pack $root\Mindscape.Raygun4Net.AspNetCore\Mindscape.Raygun4Net.AspNetCore.csproj -c Release -o $build_dir -p:PackageVersion=$version -p:FileVersion=$version
-    & dotnet pack $root\Mindscape.Raygun4Net.NetCore\Mindscape.Raygun4Net.NetCore.csproj -c Release -o $build_dir -p:PackageVersion=$version -p:FileVersion=$version
-    & dotnet pack $root\Mindscape.Raygun4Net.NetCore.Common\Mindscape.Raygun4Net.NetCore.Common.csproj -c Release -o $build_dir -p:PackageVersion=$version -p:FileVersion=$version
+    & dotnet pack $root\Mindscape.Raygun4Net.AspNetCore\Mindscape.Raygun4Net.AspNetCore.csproj -c Release -o $build_dir -p:PackageVersion=$version -p:FileVersion=$version -p:Version=$version
+    & dotnet pack $root\Mindscape.Raygun4Net.NetCore\Mindscape.Raygun4Net.NetCore.csproj -c Release -o $build_dir -p:PackageVersion=$version -p:FileVersion=$version -p:Version=$version
+    & dotnet pack $root\Mindscape.Raygun4Net.NetCore.Common\Mindscape.Raygun4Net.NetCore.Common.csproj -c Release -o $build_dir -p:PackageVersion=$version -p:FileVersion=$version -p:Version=$version
 
     # Pack nuspecs
     $nuspecs;
