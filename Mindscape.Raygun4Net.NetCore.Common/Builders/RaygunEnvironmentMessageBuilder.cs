7c31b7ca16293d9a2821d708a7196a100a7dcca0
Darcy Thomas <dthomas@raygun.com>
2023-09-11 14:01:23 +1200

    Update RaygunEnvironmentMessageBuilder.cs

--- Mindscape.Raygun4Net.NetCore.Common/Builders/RaygunEnvironmentMessageBuilder.cs
+++ Mindscape.Raygun4Net.NetCore.Common/Builders/RaygunEnvironmentMessageBuilder.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿
+using System;
+using System.IO;
 using System.Diagnostics;
 using System.Globalization;
+using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace Mindscape.Raygun4Net
 {
@@ -10,7 +14,33 @@ namespace Mindscape.Raygun4Net
     {
       RaygunEnvironmentMessage message = new RaygunEnvironmentMessage();
       
-      // The cross platform APIs for getting this information don't exist right now.
+      try
+      {
+
+        message.Architecture = RuntimeInformation.ProcessArchitecture.ToString();
+        message.OSVersion = RuntimeInformation.OSDescription;
+        message.ProcessorCount = Environment.ProcessorCount;
+        message.WindowBoundsWidth = Console.WindowWidth;
+        message.WindowBoundsHeight = Console.WindowHeight;
+        message.Cpu = Environment.GetEnvironmentVariable("PROCESSOR_IDENTIFIER");
+
+        var process = Process.GetCurrentProcess();
+
+# if NETSTANDARD2_0_OR_GREATER || NET
+
+        message.TotalVirtualMemory = (ulong)process.VirtualMemorySize64;
+        message.AvailableVirtualMemory = (ulong)process.PagedSystemMemorySize64;
+        message.TotalPhysicalMemory = (ulong)process.NonpagedSystemMemorySize64;
+       //  message.AvailablePhysicalMemory = (ulong)process.PagedSystemMemorySize64?? // Need to check these are the correct properties to map
+
+        message.DiskSpaceFree = DriveInfo.GetDrives().Select(d => (double)d.AvailableFreeSpace ).ToList();
+#endif
+        
+      }
+      catch (Exception ex)
+      {
+        Debug.WriteLine($"Failed to capture env details locale {ex.Message}");
+      }
 
       try
       {
