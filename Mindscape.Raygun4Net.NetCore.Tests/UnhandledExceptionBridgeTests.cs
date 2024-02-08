191de3ee827e7828af49effa3c39a04da8ab344c
Sean <sean@raygun.com>
2024-02-09 08:22:27 +1300

    Add test for lock upgrade/recursion issue

---
+++ Mindscape.Raygun4Net.NetCore.Tests/UnhandledExceptionBridgeTests.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Mindscape.Raygun4Net.NetCore.Tests
+{
+  [TestFixture]
+  public class UnhandledExceptionBridgeTests
+  {
+    [Test]
+    public void UnhandledExceptionBridge_WhenHandlersAreNoLongerAlive_LockExceptionsAreNotThrown()
+    {
+      Exception observedException = null;
+
+      // Need to put this into an action to cause the references to be destroyed on the GC
+      new Action(() =>
+      {
+        UnhandledExceptionBridge.OnUnhandledException(Callback);
+        UnhandledExceptionBridge.OnUnhandledException(Callback);
+        UnhandledExceptionBridge.OnUnhandledException(Callback);
+
+        void Callback(Exception e, bool b)
+        {
+        }
+      })();
+
+      // GC the callbacks, causing their handlers to be marked as not alive
+      GC.Collect();
+      GC.WaitForPendingFinalizers();
+      GC.Collect();
+      
+      try
+      {
+        // Manually for an unhandled exception, to handlers that are not alive
+        // Which should cause the lock upgrade exception
+        UnhandledExceptionBridge.RaiseUnhandledException(new Exception("Dead"), false);
+      }
+      catch (Exception ex)
+      {
+        observedException = ex;
+      }
+      
+      Assert.That(observedException, Is.Null);
+    }
+  }
+}
