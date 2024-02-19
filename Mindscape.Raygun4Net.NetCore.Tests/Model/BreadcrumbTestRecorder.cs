795fdf70b1fcc8a9640af3b028663617fa9d12dd
ProRedCat <43058406+ProRedCat@users.noreply.github.com>
2024-02-19 14:16:08 +1300

    Adds breadcrumb tests for Raygun4Net

---
+++ Mindscape.Raygun4Net.NetCore.Tests/Model/BreadcrumbTestRecorder.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+using Mindscape.Raygun4Net.Breadcrumbs;
+
+namespace Mindscape.Raygun4Net.NetCore.Tests;
+
+public class BreadcrumbTestRecorder
+{
+  [MethodImpl(MethodImplOptions.NoInlining)]
+  public void Record()
+  {
+    RaygunBreadcrumbs.Record(new RaygunBreadcrumb() { Message = "test" });
+  }
+}
