e91ed4fd22a7d8f1fe68c5f2e0352e122bc3cc22
Miguel Beltran <m@beltran.work>
2024-08-06 09:38:59 +0200

    fix: PEDebugInformation properties as nullable

--- Mindscape.Raygun4Net.Core/Diagnostics/PEDebugInformation.cs
+++ Mindscape.Raygun4Net.Core/Diagnostics/PEDebugInformation.cs
@@ -4,24 +4,25 @@ namespace Mindscape.Raygun4Net.Diagnostics;
 
 public sealed class PEDebugInformation
 {
+#nullable enable
   /// <summary>
   /// The signature of the PE and PDB linking them together - usually a GUID
   /// </summary>
-  public string Signature { get; internal set; }
+  public string? Signature { get; internal set; }
   
   /// <summary>
   /// Checksum of the PE & PDB. Format: {algorithm}:{hash:X}
   /// </summary>
-  public string Checksum { get; internal set; }
+  public string? Checksum { get; internal set; }
   
   /// <summary>
   /// The full location of the PDB at build time
   /// </summary>
-  public string File { get; internal set; }
+  public string? File { get; internal set; }
   
   /// <summary>
   /// The generated Timestamp of the code at build time stored as hex
   /// </summary>
-  public string Timestamp { get; internal set; }
+  public string? Timestamp { get; internal set; }
   
 }
