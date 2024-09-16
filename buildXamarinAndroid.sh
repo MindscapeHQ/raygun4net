a83239abc8efaac4f71342da58103b0c305bddb3
phillip-haydon <phillip.haydon@gmail.com>
2024-09-16 13:42:08 +1200

    removed legacy xamarin projects

--- buildXamarinAndroid.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-rm -rf Build/Xamarin/Android
-# Android
-"/Applications/Visual Studio.app/Contents/MacOS/vstool" -v build "--target:Clean" "--configuration:Release" "Mindscape.Raygun4Net.Xamarin.Android.sln"
-"/Applications/Visual Studio.app/Contents/MacOS/vstool" -v build "--configuration:Release" "Mindscape.Raygun4Net.Xamarin.Android.sln"
-mkdir -p Build/Xamarin/Android
-cp -v Mindscape.Raygun4Net.Xamarin.Android/bin/Release/*.dll Build/Xamarin/Android/
-cp -v Mindscape.Raygun4Net.Xamarin.Android/bin/Release/*.nupkg Build/Xamarin/Android/
--- buildXamarinMac.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-rm -rf Build/Xamarin/Mac
-# Mac
-"/Applications/Xamarin Studio.app/Contents/MacOS/mdtool" -v build "--target:Clean" "--configuration:Release" "Mindscape.Raygun4Net.Xamarin.Mac.sln"
-"/Applications/Xamarin Studio.app/Contents/MacOS/mdtool" -v build "--configuration:Release" "Mindscape.Raygun4Net.Xamarin.Mac.sln"
-mkdir -p Build/Xamarin/Mac
-cp -v Mindscape.Raygun4Net.Xamarin.Mac.Unified/bin/Release/*.dll Build/Xamarin/Mac/
-cp -v Mindscape.Raygun4Net.Xamarin.Mac.Unified/bin/Release/*.nupkg Build/Xamarin/Mac/
